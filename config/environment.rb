f67bbf8a48d9c02c1931147753c82cd544938e2d
Nico Hagenburger <nh@NH.local>
2010-03-06 14:52:12 +0100

    design

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   #config.gem 'authlogic', :version => '~> 2.0'
   config.gem 'compass', :source => 'gemcutter.org'
+  config.gem 'compass-colors', :source => 'gemcutter.org'
   config.gem 'redgreengrid', :source => 'gemcutter.org'
   config.i18n.default_locale = :de
 end
