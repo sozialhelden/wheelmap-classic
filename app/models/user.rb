5343362b5a789c2d4375a26db2baa4f35c48948d
Christoph Bünte <info@christophbuente.de>
2010-05-20 14:07:53 +0200

    merged 2 user create migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :http_authenticatable, :token_authenticatable, :confirmable, :lockable, :timeoutable and :activatable
   devise :authenticatable, :recoverable, :registerable, :rememberable, :trackable, :validatable
-  # devise :registerable, :authenticatable, :recoverable,
-  #        :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation
--- db/migrate/20100520103719_devise_create_users.rb
+++ db/migrate/20100520103719_devise_create_users.rb
@@ -1,6 +1,9 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def self.up
     create_table(:users) do |t|
+      t.string :users, :oauth_token
+      t.string :users, :oauth_secret
+      
       t.database_authenticatable :null => false
       # t.confirmable
       t.recoverable
@@ -11,6 +14,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
+    add_index :users, :oauth_token
     add_index :users, :email,                :unique => true
     # add_index :users, :confirmation_token,   :unique => true
     add_index :users, :reset_password_token, :unique => true
