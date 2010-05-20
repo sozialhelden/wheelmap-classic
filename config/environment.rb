963b99c9b1db0a8984ced15268a1eafe3da8fb8e
Christoph Bünte <info@christophbuente.de>
2010-05-20 12:21:30 +0200

    updated referenced gem version

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'compass',         :version => '0.10.0.rc1',   :source => 'rubygems.org'
+  config.gem 'compass',         :version => '0.10.0',   :source => 'rubygems.org'
   config.gem 'redgreengrid',    :version => '0.3.2',        :source => 'rubygems.org'
   config.i18n.default_locale = :de
 end
