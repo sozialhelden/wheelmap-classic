d842928921e934a05c8748547876fa4dfdfc8557
Christoph Bünte <info@christophbuente.de>
2010-06-04 12:20:25 +0200

    moved update action to node controller

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -3,4 +3,9 @@ class NodesController < ApplicationController
   def show
     @node = OpenStreetMap.get_node(params[:id])
   end
+  
+  def update
+    Delayed::Job.enqueue(UpdatingJob.new(params[:id], params[:wheelchair], default_user.id))
+    render :text => 'OK'
+  end
 end
--- lib/updating_job.rb
+++ lib/updating_job.rb
@@ -2,7 +2,7 @@ class UpdatingJob < Struct.new(:osmid, :wheelchair_status, :user_id)
   
   def perform
     user = User.find(user_id)
-    oauth = OpenStreetMap::Oauth.new(user.oauth_token, user.oauth_secret)
+    oauth = OpenStreetMap::Oauth.new(user.access_token)
     OpenStreetMap.update(osmid, oauth, wheelchair_status)
   end
   
