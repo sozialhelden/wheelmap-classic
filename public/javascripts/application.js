cf3fce9591bfcb79c726f5a05baab9a54865a93d
Christoph Bünte <info@christophbuente.de>
2010-06-14 17:08:25 +0200

    added small map to node show view

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,11 @@
 var map;
 var epsg4326, epsg900913;
-var lat = parseFloat(geoip_latitude());
-var lon = parseFloat(geoip_longitude());
+if (typeof(lat) == 'undefined'){
+  var lat = parseFloat(geoip_latitude());
+}
+if (typeof(lon) == 'undefined'){
+  var lon = parseFloat(geoip_longitude());
+}
 var zoom = 16;
 var places = [];
 var mapnik;
