5fcf85ffca20bb6a2fd1bb21c463ff0710480b0d
Christoph Bünte <info@christophbuente.de>
2010-07-02 12:23:01 +0200

    more localizations

--- config/environment.rb
+++ config/environment.rb
@@ -15,7 +15,7 @@ Rails::Initializer.run do |config|
   config.gem 'newrelic_rpm'
   config.gem 'jnunemaker-validatable', :lib => 'validatable'
   config.gem 'smurf'
-  config.i18n.default_locale = :de
+  config.i18n.default_locale = :en
 end
 
 Haml::Template.options[:format] = :html5
