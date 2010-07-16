270796fdfba0e529fde8a4c37c1c54c4eba8192f
Christoph Bünte <info@christophbuente.de>
2010-07-16 18:54:09 +0200

    enabled multi staging deploy on rocket hosting

---
+++ config/deploy/production.rb
@@ -0,0 +1,5 @@
+puts "Configuring settings for production environment"
+set :application, 'wheelmap_org'
+set :db_names, :production => 'c15wheelmap'
+set :db_user, 'c15wheelmap'
+set :db_password, '***REMOVED***'
