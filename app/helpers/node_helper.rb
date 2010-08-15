cc19f643d3dc8bdbb89b066d4531159aed86273b
Christoph Bünte <info@christophbuente.de>
2010-08-15 11:03:27 +0200

    simplified formtastic form, now with better standard error handling

--- app/helpers/node_helper.rb
+++ app/helpers/node_helper.rb
@@ -1,14 +1,10 @@
 module NodeHelper
-      
-  def options_for_wheelchair_status(current_value)
-    html = ''
+
+  def options_for_wheelchair_status
+    options = []
     WheelchairStatus.each do |status|
-      if status == current_value.to_s
-        html << "<option selected='selected' value='#{status}'>#{I18n.t('wheelchairstatus.' + status)}</option>"
-      else
-        html << "<option value='#{status}'>#{I18n.t('wheelchairstatus.' + status)}</option>"
-      end
+      options << [I18n.t('wheelchairstatus.' + status), status]
     end
-    html
+    options
   end
 end
