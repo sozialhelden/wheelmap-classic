f45d74dd6852d46ccf6cc2cc8749eb4d6429efe3
Christoph Bünte <info@christophbuente.de>
2010-08-23 07:26:35 +0200

    added hoptoad notification for failed delayed jobs

--- lib/update_single_attribute_job.rb
+++ lib/update_single_attribute_job.rb
@@ -1,8 +1,14 @@
 class UpdateSingleAttributeJob < Struct.new(:node_id, :client, :attribute_hash)
   
   def perform
-    osm = OpenStreetMap.new(client)
-    osm.update_single_attribute(node_id, attribute_hash)
+    begin
+      osm = OpenStreetMap.new(client)
+      osm.update_single_attribute(node_id, attribute_hash)
+    rescue Exception => e
+      HoptoadNotifier.notify(e, :component => 'UpdateSingleAttributeJob#perform', :parameters => {:node_id => node_id, :client => client, :attributes => attribute_hash})
+      raise e
+    end
+    
   end
   
   def on_permanent_failure
