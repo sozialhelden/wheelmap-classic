b1024ae04ba8f09d57cf5762b2ec404eb112bd4b
Christoph Bünte <info@christophbuente.de>
2010-08-25 08:26:35 +0200

    added more config stuff for delayed jobs

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,3 +1,11 @@
+Delayed::Job.destroy_failed_jobs = false
+Delayed::Job.destroy_successful_jobs = false
+silence_warnings do
+  Delayed::Job.const_set("MAX_ATTEMPTS", 25)
+  Delayed::Job.const_set("MAX_RUN_TIME", 5.minutes)
+end
+
 Delayed::Worker.destroy_failed_jobs = false
+Delayed::Worker.destroy_successful_jobs = false
 Delayed::Worker.sleep_delay = 60
 Delayed::Worker.max_attempts = 25
