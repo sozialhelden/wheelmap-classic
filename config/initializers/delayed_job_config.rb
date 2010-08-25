f1467b0e9716224628d24774888ba7de4b8190a9
Christoph Bünte <info@christophbuente.de>
2010-08-25 10:00:52 +0200

    more hoptoading

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,11 +1,5 @@
-Delayed::Job.destroy_failed_jobs = false
-Delayed::Job.destroy_successful_jobs = false
-silence_warnings do
-  Delayed::Job.const_set("MAX_ATTEMPTS", 25)
-  Delayed::Job.const_set("MAX_RUN_TIME", 5.minutes)
-end
-
 Delayed::Worker.destroy_failed_jobs = false
-Delayed::Worker.destroy_successful_jobs = false
+# Delayed::Worker.destroy_successful_jobs = false
 Delayed::Worker.sleep_delay = 60
 Delayed::Worker.max_attempts = 25
+Delayed::Worker.max_run_time = 5.minutes
