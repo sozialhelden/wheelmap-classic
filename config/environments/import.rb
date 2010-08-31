60d33f22a2980905491837e27fbfe3460f2d53c5
Christoph Bünte <info@christophbuente.de>
2010-08-31 16:08:59 +0200

    put dependencies into separate environment file

---
+++ config/environments/import.rb
@@ -0,0 +1,37 @@
+# Settings specified here will take precedence over those in config/environment.rb
+
+# The production environment is meant for finished, "live" apps.
+# Code is not reloaded between requests
+config.cache_classes = true
+
+# Full error reports are disabled and caching is turned on
+config.action_controller.consider_all_requests_local = false
+config.action_controller.perform_caching             = true
+config.action_view.cache_template_loading            = true
+
+# See everything in the log (default is :info)
+config.log_level = :debug
+
+config.gem 'libxml-ruby', :lib => 'xml/libxml'
+config.gem 'crewait'
+
+# Use a different logger for distributed setups
+# config.logger = SyslogLogger.new
+
+# Use a different cache store in production
+# config.cache_store = :mem_cache_store
+
+# Enable serving of images, stylesheets, and javascripts from an asset server
+# config.action_controller.asset_host = "http://assets.example.com"
+
+# Disable delivery errors, bad email addresses will be ignored
+# config.action_mailer.raise_delivery_errors = false
+
+# Enable threaded mode
+# config.threadsafe!
+
+Sass::Plugin.options[:style] = :compress
+
+OSM_HOST      = 'osmxapi.hypercube.telascience.org'
+OAUTH_KEY     = 'l4I6j034qeqlJ4Tqybo10w'
+OAUTH_SECRET  = 'JzCv4u2TkgrsaJqaslWvNgZtH8YM105SXlhI807G1FI'
