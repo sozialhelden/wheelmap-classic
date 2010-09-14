83cffcf80b48a4211b482e62bd7d0075461dd756
Christoph Bünte <info@christophbuente.de>
2010-09-14 23:12:47 +0200

    upgraded to rails 2.3.9 in preparation of version 3.0.0

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -194,6 +194,25 @@ function clusterStrategy(){
   return new OpenLayers.Strategy.Cluster({distance: 15, threshold: 3});
 }
 
+function bboxStrategy(){
+  return new OpenLayers.Strategy.BBOX({ratio : 1});
+}
+
+function geoJsonParser(){
+  return new OpenLayers.Format.GeoJSON({
+      internalProjection: epsg4326,
+      externalProjection: epsg4326,
+      ignoreExtraDims: true
+    });
+}
+
+function httpProtocol(){
+  return new OpenLayers.Protocol.HTTP({
+    url:  "nodes.geojson",
+    format: geoJsonParser()
+  })
+}
+
 function createPlacesLayer(style) { 
   places = new OpenLayers.Layer.Vector(
     "Places ",
@@ -202,15 +221,8 @@ function createPlacesLayer(style) {
       projection: epsg4326,
       displayProjection: epsg4326,
       rendererOptions: { yOrdering: true },
-      // strategies: [new OpenLayers.Strategy.BBOX({ratio : 1})],
-      // protocol: new OpenLayers.Protocol.HTTP({
-      //   url:  "nodes.geojson",
-      //   format: new OpenLayers.Format.GeoJSON({
-      //     internalProjection: epsg4326,
-      //     externalProjection: epsg4326,
-      //     ignoreExtraDims: true
-      //   })
-      // }),
+      // strategies: [bboxStrategy()],
+      // protocol: httpProtocol(),
       visibility: true,
     });
   map.addLayer(places);
@@ -238,9 +250,9 @@ function activateDragControl(layer){
 }
 
 function activateSelectControl(layer){
-  var sc = new OpenLayers.Control.SelectFeature(layer, { onSelect:openPopup});
-  map.addControl(sc);
-  sc.activate();  
+  var control = new OpenLayers.Control.SelectFeature(layer, {onSelect:openPopup});
+  map.addControl(control);
+  control.activate();
 }
 
 
