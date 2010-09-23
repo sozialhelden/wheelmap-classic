7350956967ba740e27eca743b18d9f7b9d0ebea1
Christoph Bünte <info@christophbuente.de>
2010-09-23 10:42:23 +0200

    set default host name for password reset mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,8 @@ config.action_view.cache_template_loading            = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default_url_options = { :host => 'wheelmap.org' }
+
 
 # Enable threaded mode
 # config.threadsafe!
