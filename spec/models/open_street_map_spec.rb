1c393bc5e1834b97e1d4f960b143e4a32d2a22ef
Christoph Bünte <info@christophbuente.de>
2010-09-24 12:20:23 +0200

    fixed specs

--- spec/models/open_street_map_spec.rb
+++ spec/models/open_street_map_spec.rb
@@ -2,23 +2,30 @@ require 'spec_helper'
 describe OpenStreetMap do
   
   before(:each) do
+    FakeWeb.allow_net_connect = false
     @base_url = "#{OpenStreetMapConfig.oauth_site}/api/0.6"
-    @basic_auth_client = OpenStreetMap::BasicAuthClient.new('foo', 'bar')
     uri = URI.parse("#{OpenStreetMapConfig.oauth_site}/api/0.6")
+
+    @basic_auth_client = OpenStreetMap::BasicAuthClient.new('foo', 'bar')
     @basic_auth_url = "#{uri.scheme}://foo:bar@#{uri.host}#{uri.path}"
     
-    FakeWeb.allow_net_connect = false
     @consumer = ::OAuth::Consumer.new(OpenStreetMapConfig.oauth_key, OpenStreetMapConfig.oauth_secret, :site => @base_url)
     @access_token = ::OAuth::AccessToken.new(@consumer, 'foo', 'bar')
+
     @oauth_client = OpenStreetMap::OauthClient.new(@access_token)
-    @oauth_osm = OpenStreetMap.new(@client)
-    @basic_auth_osm = OpenStreetMap.new(@client)
+    @oauth_url = uri
+
+    @osm = OpenStreetMap.new(@oauth_client)
+
     
   end
   
   after(:each) do
     FakeWeb.clean_registry
   end
+  
+  
+  
   
   describe 'method: get_node' do
   
@@ -49,11 +56,11 @@ describe OpenStreetMap do
   
   describe "method: create_changeset" do
     before(:each) do
-      @full_url = "#{@basic_auth_url}/changeset/create"
+      @full_url = "#{@oauth_url}/changeset/create"
     end
 
     it "should create a new changeset" do
-      FakeWeb.register_uri(:put, @full_url, :body => "12345", :content_type => 'text/plain', :basic_auth => @client.credentials)
+      FakeWeb.register_uri(:put, @full_url, :body => "12345", :content_type => 'text/plain')
       changeset_id = @osm.create_changeset
       changeset_id.should == 12345
     end
