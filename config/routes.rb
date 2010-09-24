f8c54322cef1db73ddb6cee3284e81aaeb401897
Christoph Bünte <info@christophbuente.de>
2010-09-24 17:08:50 +0200

    minor optical improvements

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,25 @@
 ActionController::Routing::Routes.draw do |map|
 
   map.root :controller => 'home', :action => 'index'
-  map.imprint '/imprint', :controller => 'pages', :action => 'show', :id => 'imprint'
-
+  
   map.devise_for :admins
 
   map.devise_for :users
-  
-  map.resources :user, :only => :new # Fake route for redirection to OSM register page
-  
+
   map.resources :search, :only => :index
-    
+
   map.resources :oauth, :only => [:new, :index], :collection => { :callback => :get, :osm_register => :get }
-  
+
   map.resources :nodes, :except => :destroy, :member => {:update_wheelchair => :put}
-  
+
   map.resources :users, :as => 'profile'
 
   map.resources :users, :collection => {:authenticate => :post}
-  
+
+  map.resources :user, :only => :new # Fake route for redirection to OSM register page
+
+  map.imprint '/imprint', :controller => 'pages', :action => 'show', :id => 'imprint'
+
   #map.connect ':controller/:action/:id'
   #map.connect ':controller/:action/:id.:format'
 end
