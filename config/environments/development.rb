3038ab546a11bf6f2fc85dc3803ba8766d56c8a9
Christoph Bünte <info@christophbuente.de>
2010-09-24 12:45:21 +0200

    enabled smtp delivery

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,8 +14,9 @@ config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
-config.action_mailer.raise_delivery_errors = false
-config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+# config.action_mailer.raise_delivery_errors = false
+config.action_mailer.delivery_method = :test
+config.action_mailer.default_url_options = { :host => 'wheelmap.local' }
 
 OSM_HOST      = 'api06.dev.openstreetmap.org'
 OAUTH_KEY     = 'PdVEU2YnFTwLR9vFzho0xQ'
