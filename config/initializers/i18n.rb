6dc75ef7f44d776c5423428dae60d381236b5622
Christoph Bünte <info@christophbuente.de>
2010-10-08 14:30:06 +0200

    fixed tolk views

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,3 +1,4 @@
 # # tell the I18n library where to find your translations
 I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 I18n.default_locale = :de
+Tolk::Locale.primary_locale_name = 'en'
