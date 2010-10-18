f3d7f08e8f3ce5ebe0dab958355b7aebc80d7be4
Christoph Bünte <info@christophbuente.de>
2010-10-18 20:57:03 +0200

    default zoom is 18

--- lib/updating_job.rb
+++ lib/updating_job.rb
@@ -2,15 +2,18 @@ class UpdatingJob < Struct.new(:node, :client)
   
   def perform
     begin
+      lat = node.lat
+      lon = node.lon
       OpenStreetMap.logger = Delayed::Worker.logger
       old_node = OpenStreetMap.get_node(node.id)
       old_tags = old_node.tags
       Delayed::Worker.logger.debug("OLD TAGS: #{old_tags.to_yaml}")
-      new_tags = node.tags
+      new_tags = node.tags.reverse_merge(old_tags)
       Delayed::Worker.logger.debug("NEW TAGS: #{new_tags.to_yaml}")
-      new_tags.reverse_merge!(old_tags)
       node = old_node.clone
       node.tags = new_tags
+      node.lat = lat
+      node.lon = lon
       # if old_node == node
       #   # Quit here, if there are no changes to be made
       #   HoptoadNotifier.notify(Exception.new('No update needed'), :component => 'UpdatingJob#perform', :parameters => {:old_node => old_node.inspect, :node => node.inspect, :client => client})
