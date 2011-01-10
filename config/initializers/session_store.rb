c95ec49da64713bfd133b57f23765d567d90cd88
Christoph Bünte <info@christophbuente.de>
2011-01-10 07:30:44 +0100

    - added formtastic - added google streetview config - use new syntax for I18n interpolation - make sure html_safe is used to prevent save code to be escaped

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Wheelmap::Application.config.session_store :cookie_store, :key => '_wheelmap_session'
+Wheelmap::Application.config.session_store :cookie_store, :key => '_wheelmap_session', :secret      => 'e7d8c7242ead21d933a7511ebb64d1b048d6a34b33786fbcee59e059096734b90858dd3e42865148ce895d1d21676aa22952badb04a5248e37c9fad63b0989ec'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Wheelmap::Application.config.session_store :active_record_store
+Wheelmap::Application.config.session_store :active_record_store
