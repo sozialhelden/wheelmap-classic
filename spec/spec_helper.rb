074102cdce2087dbb8513f9569508b5886f40146
Andreas Korth <andreas.korth@gmail.com>
2011-01-19 17:55:19 +0100

    Added spec for OAuth controller. Added WebMock.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'factory_girl'
+require 'webmock/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
