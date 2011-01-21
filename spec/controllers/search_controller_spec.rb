4f2ffa31fb86c95ca5e2327ddf0498d5ea846aa3
Andreas Korth <andreas.korth@gmail.com>
2011-01-21 11:28:15 +0100

    Replaced FakeWeb with WebMock in search_controller_spec.

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -4,13 +4,12 @@ include REXML
 
 describe SearchController do
   
-  before :each do
-    FakeWeb.allow_net_connect = false
-  end
-  
   it "should call the OSM search API with xml format" do
     @search_api_url = "http://nominatim.openstreetmap.org/search?q=Leipziger+Strasse%2C+Berlin&format=xml&limit=10&accept-language=de&email=info%40wheelmap.org&osm_type=N"
-    FakeWeb.register_uri(:get, @search_api_url, :body => "#{Rails.root}/spec/fixtures/search.xml", :content_type => 'text/xml')
+    
+    stub_request(:get, @search_api_url).
+      to_return(:status => 200, :body => File.read("#{Rails.root}/spec/fixtures/search.xml"))
+    
     get( :index, :q => 'Leipziger Strasse, Berlin', :format => 'xml', :'accept-language' => 'de', :osm_type => 'N')
     response.should be_success
     doc = Document.new(response.body)
