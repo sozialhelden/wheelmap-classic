c9c5fd8527455395fe085340321360df1ab6fbea
Christoph Bünte <info@christophbuente.de>
2011-01-28 20:24:37 +0100

    put back tu minutely

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,7 +4,7 @@ set :output, File.join(File.expand_path(File.dirname(__FILE__)), "log", "cron_lo
 
 job_type :rake,    "cd :path && RAILS_ENV=:environment /home/rails/.rvm/bin/rake :task :output"
 
-every 1.minute :at => [0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] do
+every 1.minute do
   rake "osm:replication:sync", :environment => :staging
 end
 
