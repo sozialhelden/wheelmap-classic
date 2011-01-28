7c07e8facac41f4777fb87a386035d5244f8e84a
Andreas Korth <andreas.korth@gmail.com>
2011-01-28 17:51:22 +0100

    Updated spatial adapter gem and deployment script.

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,8 +2,8 @@ set :stages,        %w(staging production)
 set :default_stage, "staging"
 require 'capistrano/ext/multistage'
 
-set :whenever_command, "bundle exec whenever"
-require "whenever/capistrano"
+# set :whenever_command, "bundle exec whenever"
+# require "whenever/capistrano"
 
 set :application, "wheelmap"
 set :repository,  "git@github.com:sozialhelden/wheelmap.git"
