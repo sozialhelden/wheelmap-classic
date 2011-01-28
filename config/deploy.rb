beec20733025a5c96d8bfafdad3a32f381ec463a
Christoph Bünte <info@christophbuente.de>
2011-01-28 12:46:06 +0100

    deployable to new server

---
+++ config/deploy.rb
@@ -0,0 +1,57 @@
+set :stages,        %w(staging production)
+set :default_stage, "staging"
+require 'capistrano/ext/multistage'
+
+set :application, "wheelmap"
+set :repository,  "git@github.com:sozialhelden/wheelmap.git"
+
+set :branch, ENV['BRANCH'] || "master"
+
+set :use_sudo, false
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+set :git_shallow_clone, 1
+
+set :deploy_via, :remote_cache
+set :default_run_options, :pty => true # or else you'll get "sorry, you must have a tty to run sudo"
+
+set :ssh_options, :keys => [ File.expand_path("~/.ssh/wheelmap_rsa") ], :forward_agent => true
+
+set :user, 'rails'
+
+role :web, "178.77.98.117"                          # Your HTTP server, Apache/etc
+role :app, "178.77.98.117"                          # This may be the same as your `Web` server
+role :db,  "178.77.98.117", :primary => true # This is where Rails migrations will run
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+after  'deploy:symlink',  'deploy:symlink_configs'
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  task :symlink_configs do
+    run "mkdir -p #{shared_path}/config/"
+    %w(database.yml osm.yml open_street_map.yml).each do |file|
+      run "ln -nfs #{shared_path}/config/#{file} #{release_path}/config/#{file}"
+    end
+  end
+end
+
+# have builder check and install gems after each update_code
+require 'bundler/capistrano'
+set :bundle_without, [:development, :test, :metrics, :deployment]
+
+
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"
+set :rvm_ruby_string, 'ruby-1.8.7-p330'
+set :rvm_type, :user
