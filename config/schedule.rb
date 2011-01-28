7dbc16e0a514348c0fa95749c4a53c4913085af6
Christoph Bünte <info@christophbuente.de>
2011-01-28 15:23:55 +0100

    set environment

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,9 @@
 # Use this file to easily define all of your cron jobs.
 
 set :output, File.join(File.expand_path(File.dirname(__FILE__)), "log", "cron_log.log")
+set :path, '/home/rails/.rvm/bin/'
 
-every 1.minute, :environment => 'staging' do
-  rake "osm:replication:sync"
+every 1.minute do
+  rake "osm:replication:sync", :environment => :staging
 end
 
