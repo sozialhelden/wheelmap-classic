35204c50e15f6b6bc6f3a9fbcb2b9eabd9873ee5
Andreas Korth <andreas.korth@gmail.com>
2011-01-28 14:14:44 +0100

    Added whenever gem.

---
+++ config/schedule.rb
@@ -0,0 +1,8 @@
+# Use this file to easily define all of your cron jobs.
+
+set :output, File.join(File.expand_path(File.dirname(__FILE__)), "log", "cron_log.log")
+
+every 1.minute do
+  rake "osm:replication:sync"
+end
+
