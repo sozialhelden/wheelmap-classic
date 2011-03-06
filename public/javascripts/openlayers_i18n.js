ba38b4daae8a00315805b7f0591505469730046e
Christoph Bünte <info@christophbuente.de>
2011-03-06 22:57:14 +0100

    added i18n translations to openlayers

--- public/javascripts/openlayers_i18n.js
+++ public/javascripts/openlayers_i18n.js
@@ -14,11 +14,43 @@ OpenLayers.Lang.en = {
   'wheelchair_update_button': 'Update',
   'more_information': 'more ...'
 };
+OpenLayers.Lang.es = { 
+  'wheelchair_label_yes': 'accesible en silla de ruedas',
+  'wheelchair_label_limited': 'acceso medio en silla de ruedas',
+  'wheelchair_label_no': 'sin acceso en silla de ruedas',
+  'wheelchair_label_unknown': 'se desconoce el estado',
+  'wheelchair_update_button': 'Guardar',
+  'more_information': 'más ...'
+};
+OpenLayers.Lang.fr = { 
+  'wheelchair_label_yes': 'accès pour fauteuil roulant',
+  'wheelchair_label_limited': 'accès partiel pour fauteuil roulant',
+  'wheelchair_label_no': "pas d'accès pour fauteuil roulant",
+  'wheelchair_label_unknown': 'statut inconnu',
+  'wheelchair_update_button': 'Sauvegarder',
+  'more_information': 'davantage ...'
+};
+OpenLayers.Lang.it = { 
+  'wheelchair_label_yes': 'accessibile in sedia a rotelle',
+  'wheelchair_label_limited': 'parzialmente accessibile in sedia a rotelle',
+  'wheelchair_label_no': 'non accessibile in sedia a rotelle',
+  'wheelchair_label_unknown': 'stato sconosciuto',
+  'wheelchair_update_button': 'Salva',
+  'more_information': 'più ...'
+};
+OpenLayers.Lang.is = { 
+  'wheelchair_label_yes': 'hjólastólaaðgengilegt',
+  'wheelchair_label_limited': 'að hluta til hjólastólaaðgengilegt',
+  'wheelchair_label_no': 'ekki hjólastólaaðgengilegt',
+  'wheelchair_label_unknown': 'óvituð staða',
+  'wheelchair_update_button': 'vista',
+  'more_information': 'meiri ...'
+};
 OpenLayers.Lang.jp = { 
   'wheelchair_label_yes': '車椅子可',
   'wheelchair_label_limited': '車椅子一部可',
   'wheelchair_label_no': '車椅子不可',
   'wheelchair_label_unknown': '不明',
   'wheelchair_update_button': '保存',
-  'more_information': 'more ...'
+  'more_information': '詳細 ...'
 };
