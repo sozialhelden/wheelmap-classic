c2dbe4c761f738007413706044c0aec845a2123f
Christoph Bünte <info@christophbuente.de>
2011-03-18 10:31:19 +0100

    removed already registered mime type

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -5,5 +5,4 @@
 # Mime::Type.register_alias "text/html", :iphone
 Mime::Type.register_alias "application/json; subtype=geojson", :geojson
 Mime::Type.register_alias "application/vnd.google-earth.kml+xml", :kml
-Mime::Type.register_alias "application/rss+xml", :rss
 Mime::Type.register_alias "application/xml", :sitemap
