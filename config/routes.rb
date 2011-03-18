ddf797244b9e4138400b3d412665c9d6b6299048
Christoph Bünte <info@christophbuente.de>
2011-03-18 11:46:38 +0100

    added features for I18n locales

--- config/routes.rb
+++ config/routes.rb
@@ -1,44 +1,47 @@
 Wheelmap::Application.routes.draw do
+
   filter :locale
   RoutingFilter::Locale.include_default_locale = false
+  # scope "(:locale)", :locale => /de|en|es|fr|jp|is|it/ do
   
-  root :to => 'home#index'
+    root :to => 'home#index'
   
-  devise_for :admins
-  devise_for :users
+    devise_for :admins
+    devise_for :users
 
-  resources :search, :only => :index
-  resources :feeds, :only => :index
-  resources :sitemap, :only => :index
-  resources :oauth, :only => [:new, :index] do
-    collection do 
-      get :revoke
-      get :callback
-      get :osm_register
+    resources :search, :only => :index
+    resources :feeds, :only => :index
+    resources :sitemap, :only => :index
+    resources :oauth, :only => [:new, :index] do
+      collection do 
+        get :revoke
+        get :callback
+        get :osm_register
+      end
     end
-  end
 
-  resources :nodes, :except => :destroy do
-    member do 
-      put :update_wheelchair
+    resources :nodes, :except => :destroy do
+      member do 
+        put :update_wheelchair
+      end
+      collection do
+        get :sitemap
+      end
     end
-    collection do
-      get :sitemap
+
+    resources :users, :as => 'profile', :path => '/profile'
+    resources :users do
+      collection do 
+        post :authenticate
+      end
     end
-  end
 
-  resources :users, :as => 'profile', :path => '/profile'
-  resources :users do
-    collection do 
-      post :authenticate
-    end
-  end
+    resources :user, :only => :new # Fake route for redirection to OSM register page
 
-  resources :user, :only => :new # Fake route for redirection to OSM register page
+    match '/imprint' => 'pages#show'
 
-  match '/imprint' => 'pages#show'
-
-  # match a single given locale and send it to the root url
-  match '/:locale' => 'home#index'
+    # match a single given locale and send it to the root url
+    match '/:locale' => 'home#index'
+  # end
 
 end
