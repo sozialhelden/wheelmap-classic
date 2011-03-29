ec29a94328bcffa9b32f57a3f328d86b3c081008
Christoph Bünte <info@christophbuente.de>
2011-03-29 07:13:00 +0200

    fixed cron syntax

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,8 +5,12 @@ set :output, File.join(File.expand_path(File.dirname(__FILE__)), "log", "cron_lo
 job_type :rake,    "cd :path && RAILS_ENV=:environment /home/rails/.rvm/bin/rake :task :output"
 job_type :command, "cd :path && :task :output"
 
-# Sync with OSM 
-every '* 0,1,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *' do
+# Sync with OSM but not between 1:59 and 3:00 o'clock
+every '* 0-1 * * *' do
+  rake "osm:replication:sync", :environment => :production
+end
+
+every '* 3-23 * * *' do
   rake "osm:replication:sync", :environment => :production
 end
 
