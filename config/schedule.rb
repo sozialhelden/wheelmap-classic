84f1ffa68c0ce13e2ba257be73a65a673e5aa3b1
Christoph Bünte <info@christophbuente.de>
2011-03-29 07:06:03 +0200

    removed cached files via cronjob

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,9 +3,15 @@
 set :output, File.join(File.expand_path(File.dirname(__FILE__)), "log", "cron_log.log")
 
 job_type :rake,    "cd :path && RAILS_ENV=:environment /home/rails/.rvm/bin/rake :task :output"
+job_type :command, "cd :path && :task :output"
 
-
+# Sync with OSM 
 every '* 0-1,3-23 * * *' do
   rake "osm:replication:sync", :environment => :production
+end
+
+# Remove cached files older than 3 days
+every '* * * * *' do
+  command "find tmp/cache/ -mmin +4320 -type f -delete", :environment => :production
 end
 
