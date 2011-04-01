1fe518b1f4605591487daae0b4511e92afad44a6
Christoph Bünte <info@christophbuente.de>
2011-04-01 20:59:34 +0200

    Fix google streetview api_key issue.

--- config/initializers/google.rb
+++ config/initializers/google.rb
@@ -1,4 +1,4 @@
 require 'ostruct'
 require 'yaml'
 google_config = YAML.load_file("#{Rails.root}/config/google.yml")
-GoogleConfig = OpenStruct.new google_config[Rails.env.to_sym]
+GoogleConfig = OpenStruct.new google_config[Rails.env]
