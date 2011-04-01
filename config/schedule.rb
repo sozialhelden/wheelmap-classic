54cce2513ad437fa121ca11edd05b030f72891a4
Christoph Bünte <info@christophbuente.de>
2011-04-01 13:12:21 +0200

    delete cache files

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,6 @@
 set :output, File.join(File.expand_path(File.dirname(__FILE__)), "log", "cron_log.log")
 
 job_type :rake,    "cd :path && RAILS_ENV=:environment /home/rails/.rvm/bin/rake :task :output"
-job_type :command, "cd :path && :task :output"
 
 # Sync with OSM but not between 1:59 and 3:00 o'clock
 every '* 0-1 * * *' do
@@ -16,6 +15,6 @@ end
 
 #Remove cached files older than 3 days
 every 1.minute do
- command "find tmp/cache/ -mmin +4320 -type f -delete", :environment => :production
+ command "find #{:path}/tmp/cache/ -mmin +4320 -type f -delete", :environment => :production
 end
 
