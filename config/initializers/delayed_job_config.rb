2fc4a149694d4b09e4164b47fb4848b554a0a283
Christoph Bünte <info@christophbuente.de>
2011-04-11 22:39:08 +0200

    Set Rails logger as delayed job logger

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -3,3 +3,4 @@ Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.sleep_delay = 60
 Delayed::Worker.max_attempts = 25
 Delayed::Worker.max_run_time = 5.minutes
+Delayed::Worker.logger = Rails.logger
