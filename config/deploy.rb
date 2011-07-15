c7c3d49d9db714c7c9c15584110adb17f3c900a6
Christoph Bünte <info@christophbuente.de>
2011-07-15 14:06:32 +0200

    Do not exclude danish for live translations.

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,7 +51,7 @@ namespace :deploy do
   
   task :remove_all_unfinished_locales do
     if rails_env == :production
-      %w(da pt-PT ru zh pt).each do |locale|
+      %w(pt-PT ru zh pt).each do |locale|
         run "rm -f #{release_path}/config/locales/#{locale}.yml"
       end
     end
