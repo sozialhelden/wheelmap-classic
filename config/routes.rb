d968dba588dd0bac9a4109a07b915eadc5fa5bab
Christoph Bünte <info@christophbuente.de>
2011-07-22 14:31:15 +0200

    More and nicer API docs.

--- config/routes.rb
+++ config/routes.rb
@@ -57,13 +57,13 @@ Wheelmap::Application.routes.draw do
       end
     end
 
-    resources :categories,  :only  => [:index, :show] do
-      resources :nodes,       :only  => [:index, :show] do
+    resources :categories,   :only  => [:index, :show] do
+      resources :node_types, :only  => [:index, :show]
+      resources :nodes,      :only  => [:index, :show] do
         collection do
           get :search
         end
       end
-      resources :node_types, :only  => [:index, :show]
     end
     
     resources :node_types, :only  => [:index, :show] do
