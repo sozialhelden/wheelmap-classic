6da8aa5891f7bed460bf18b7e1c2ad4dfa764c6d
Christoph Bünte <info@christophbuente.de>
2011-08-04 16:03:13 +0200

    Add local staging server.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -25,3 +25,17 @@ set :rvm_bin_path, "/home/rails/.rvm/bin"
 # set :rvm_type, :system
 # set :rvm_bin_path, "/usr/local/rvm/bin"
 # 
+# >>>>>>>>>>>>>>>>>>>>>> NEW SERVER
+# role :web, "10.211.55.9"                          # Your HTTP server, Apache/etc
+# role :app, "10.211.55.9"                          # This may be the same as your `Web` server
+# role :db,  "10.211.55.9", :primary => true # This is where Rails migrations will run
+#
+# set :deploy_to, "/var/apps/wheelmap/staging"
+# set :rails_env, :staging
+#
+# $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+# require "rvm/capistrano"
+# set :rvm_ruby_string, 'ree-1.8.7-2011.03'
+# set :rvm_type, :system
+# set :rvm_bin_path, "/usr/local/rvm/bin"
+#
