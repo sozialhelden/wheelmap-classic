49c6728d0556baabde9633d8547b2329254fe824
Christoph Bünte <info@christophbuente.de>
2011-08-05 22:33:12 +0200

    Rescue migration.

--- db/migrate/20110717073922_add_node_type_id_to_pois.rb
+++ db/migrate/20110717073922_add_node_type_id_to_pois.rb
@@ -1,7 +1,7 @@
 class AddNodeTypeIdToPois < ActiveRecord::Migration
   def self.up
-    add_column :pois, :node_type_id, :integer
-    add_index  :pois, [:node_type_id, :osm_id]
+    add_column :pois, :node_type_id, :integer rescue nil
+    add_index  :pois, [:node_type_id, :osm_id] rescue nil
   end
 
   def self.down
