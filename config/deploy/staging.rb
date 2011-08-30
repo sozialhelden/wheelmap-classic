a7be3e2e1de4042f894145fe47e38cbc91342696
Christoph Bünte <info@christophbuente.de>
2011-08-30 16:56:16 +0200

    New deployment script for Hetzner Server.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,41 +1,25 @@
-role :web, "178.77.98.117"                          # Your HTTP server, Apache/etc
-role :app, "178.77.98.117"                          # This may be the same as your `Web` server
-role :db,  "178.77.98.117", :primary => true # This is where Rails migrations will run
-
-set :deploy_to, "/var/www/rails/staging_wheelmap_org"
 set :rails_env, :staging
+
+role :web, "88.198.52.240"                          # Your HTTP server, Apache/etc
+role :app, "88.198.52.240"                          # This may be the same as your `Web` server
+role :db,  "88.198.52.240", :primary => true # This is where Rails migrations will run
+set :port, 22022
+set :deploy_to, "/var/apps/wheelmap/staging"
 
 $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 require "rvm/capistrano"
-set :rvm_ruby_string, 'ruby-1.8.7-p330'
-set :rvm_type, :user
-set :rvm_bin_path, "/home/rails/.rvm/bin"
+set :rvm_ruby_string, 'ree-1.8.7-2011.03'
+set :rvm_type, :system
+set :rvm_bin_path, "/usr/local/rvm/bin"
 
-# >>>>>>>>>>>>>>>>>>>>>> NEW SERVER
-# role :web, "88.198.52.240"                          # Your HTTP server, Apache/etc
-# role :app, "88.198.52.240"                          # This may be the same as your `Web` server
-# role :db,  "88.198.52.240", :primary => true # This is where Rails migrations will run
-# 
-# set :deploy_to, "/var/apps/wheelmap/staging"
-# set :rails_env, :staging
-# 
-# $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
-# require "rvm/capistrano"
-# set :rvm_ruby_string, 'ree-1.8.7-2011.03'
-# set :rvm_type, :system
-# set :rvm_bin_path, "/usr/local/rvm/bin"
-# 
-# >>>>>>>>>>>>>>>>>>>>>> NEW SERVER
+# >>>>>>>>>>>>>>>>>>>>>> Virtual test server
 # role :web, "10.211.55.9"                          # Your HTTP server, Apache/etc
 # role :app, "10.211.55.9"                          # This may be the same as your `Web` server
 # role :db,  "10.211.55.9", :primary => true # This is where Rails migrations will run
-#
 # set :deploy_to, "/var/apps/wheelmap/staging"
-# set :rails_env, :staging
-#
+# 
 # $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 # require "rvm/capistrano"
 # set :rvm_ruby_string, 'ree-1.8.7-2011.03'
 # set :rvm_type, :system
 # set :rvm_bin_path, "/usr/local/rvm/bin"
-#
