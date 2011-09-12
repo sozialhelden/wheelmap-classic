a28e6add2acee799f54067eea72970b44fb77f7f
Christoph Bünte <info@christophbuente.de>
2011-09-12 17:49:33 +0200

    Use memory store fir I18n caching.

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -2,4 +2,4 @@
 I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 I18n.default_locale = :de
 I18n::Backend::Simple.send(:include, I18n::Backend::Cache)
-I18n.cache_store = ActiveSupport::Cache.lookup_store(:mem_cache_store)
+I18n.cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
