efbb30db184b77c9945b8afd62a3fd5113bbe35e
Christoph Bünte <info@christophbuente.de>
2011-09-14 18:55:08 +0200

    Overhaul of start page.

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,5 +1,7 @@
 # # tell the I18n library where to find your translations
 I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 I18n.default_locale = :de
-I18n::Backend::Simple.send(:include, I18n::Backend::Cache)
-I18n.cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
+unless Rails.env.development?
+  I18n::Backend::Simple.send(:include, I18n::Backend::Cache)
+  I18n.cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
+end
