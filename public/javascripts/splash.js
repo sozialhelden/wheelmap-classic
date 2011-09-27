bd5362c8389f87b798bb3eecd41d74ba6c7d537f
Enno Brehm <enno.brehm@gmail.com>
2011-09-27 16:52:45 +0200

    Set cookie also if "What is.." is clicked.

--- public/javascripts/splash.js
+++ public/javascripts/splash.js
@@ -2,24 +2,34 @@ $(function() {
   var cookieName = '_wheelmap_splash_seen';
 
   if(!$.cookie(cookieName)) {
-    wwidth = $(window).width();
-    left = (0.5 - 300/wwidth) * 100 + '%';
+    width = 600; // splash width
+
+    // calculate left edge so it is centered
+    left = (0.5 - (width / 2)/($(window).width())) * 100 + '%';
 
     $.blockUI({
       message: $("#splash"),
       css: {
               top: '25%',
               left: left,
-              width: '600px'
+              width: width + 'px'
            }
     });
 
-    $("#splash .unblock-splash").click($.unblockUI)
-
-    $('.blockOverlay').css('cursor', 'auto').click(function() {
+    var clickHandler = function() {
       $.unblockUI();
-      $.cookie(cookieName, true)
+      $.cookie(cookieName, true);
+      return false;
+    };
+
+    $("#splash .unblock-splash").click(clickHandler);
+    $('.blockOverlay').css('cursor', 'auto').click(clickHandler);
+
+    $('a.whatis').click(function() {
+      $.cookie(cookieName, true);
+      return true;
     });
+
   }
 
 })
