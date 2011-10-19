002dfe57bab87459e6054b72247f01dbd37b69cf
Christoph Bünte <info@christophbuente.de>
2011-10-19 14:12:55 +0200

    Error handling landing pages.

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -10,15 +10,21 @@ class LandingPagesController < ApplicationController
   end
 
   def load_region
-    @region = Region.find params[:region_id]
+    @region = Region.find params[:region_id] rescue nil
+    @message = "Could not find region #{params[:region_id]}"
+    render :template => 'shared/error', :status => 400 unless @region
   end
 
   def load_status
     @status = Poi::WHEELCHAIR_STATUS_VALUES[params[:wheelchair].to_sym]
+    @message = "Could not find status #{params[:wheelchair]}"
+    render :template => 'shared/error', :status => 400 unless @status
   end
 
   def load_type
     @node_type = NodeType.find_by_identifier(params[:node_type_id])
+    @message = "Could not find type #{params[:node_type_id]}"
+    render :template => 'shared/error', :status => 400 unless @node_type
   end
 
 end
