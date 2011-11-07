53d7e923784ae33574bb94e854e39932d830d13c
Christoph Bünte <info@christophbuente.de>
2011-11-07 17:46:51 +0100

    Add task to generate I18n.js file from yml translations.

--- public/javascripts/openlayers_i18n.js
+++ public/javascripts/openlayers_i18n.js
@@ -1,110 +1,117 @@
-OpenLayers.Lang.da = { 
-  'wheelchair_label_yes': 'Fuldt kørestoltilgængeligt',
-  'wheelchair_label_limited': 'delvis kørestolstilgængeligt',
-  'wheelchair_label_no': 'Ikke kørestolstilgængeligt',
-  'wheelchair_label_unknown': 'ukendt',
-  'wheelchair_update_button': 'Gem',
-  'more_information': 'mere ...',
-  'wheelchair_help': 'Kørestolstilgængeligt? (Hjælp)'
+OpenLayers.Lang.da = {
+  'wheelchair_label_yes': "Fuldt kørestoltilgængeligt",
+  'wheelchair_label_limited': "delvis kørestolstilgængeligt",
+  'wheelchair_label_no': "Ikke kørestolstilgængeligt",
+  'wheelchair_label_unknown': "ukendt",
+  'wheelchair_update_button': "Gem",
+  'more_information': "mere ...",
+  'wheelchair_help': "Kørestolstilgængeligt? (Hjælp)"
 };
-OpenLayers.Lang.de = { 
-  'wheelchair_label_yes': 'rollstuhlgerecht',
-  'wheelchair_label_limited': 'teilweise rollstuhlgerecht',
-  'wheelchair_label_no': 'nicht rollstuhlgerecht',
-  'wheelchair_label_unknown': 'unbekannt',
-  'wheelchair_update_button': 'Speichern',
-  'more_information': 'mehr ...',
-  'wheelchair_help': 'Rollstuhlgerecht? (Hilfe)'
+OpenLayers.Lang.de = {
+  'wheelchair_label_yes': "Voll Rollstuhlgerecht",
+  'wheelchair_label_limited': "Teilweise rollstuhlgerecht",
+  'wheelchair_label_no': "Nicht rollstuhlgerecht",
+  'wheelchair_label_unknown': "Unbekannter Status",
+  'wheelchair_update_button': "Speichern",
+  'more_information': "mehr ...",
+  'wheelchair_help': "Rollstuhlgerecht? (Hilfe)"
 };
-OpenLayers.Lang.el = { 
-  'wheelchair_label_yes': 'Κατάλληλο για αναπηρική καρέκλα',
-  'wheelchair_label_limited': 'Εν μέρη προσβάσιμο με αναπηρική καρέκλα',
-  'wheelchair_label_no': 'Ακατάλληλο για αναπηρική καρέκλα',
-  'wheelchair_label_unknown': 'Κατάσταση άγνωστη',
-  'wheelchair_update_button': 'Ενημέρωση',
-  'more_information': 'περισσότερα ...',
-  'wheelchair_help': 'Δυνατότητα πρόσβασης με αναπηρική καρέκλα'
+OpenLayers.Lang.el = {
+  'wheelchair_label_yes': "Κατάλληλο για αναπηρική καρέκλα",
+  'wheelchair_label_limited': "Εν μέρη προσβάσιμο με αναπηρική καρέκλα",
+  'wheelchair_label_no': "Ακατάλληλο για αναπηρική καρέκλα",
+  'wheelchair_label_unknown': "Κατάσταση άγνωστη",
+  'wheelchair_update_button': "Ενημέρωση",
+  'more_information': "περισσότερα ...",
+  'wheelchair_help': "Δυνατότητα πρόσβασης με αναπηρική καρέκλα"
 };
-OpenLayers.Lang.en = { 
-  'wheelchair_label_yes': 'wheelchair accessible',
-  'wheelchair_label_limited': 'partly wheelchair accessible',
-  'wheelchair_label_no': 'not wheelchair accessible',
-  'wheelchair_label_unknown': 'unknown status',
-  'wheelchair_update_button': 'Update',
-  'more_information': 'more ...',
-  'wheelchair_help': 'Wheelchair accessible? (Help)'
+OpenLayers.Lang.en = {
+  'wheelchair_label_yes': "wheelchair accessible",
+  'wheelchair_label_limited': "partly wheelchair accessible",
+  'wheelchair_label_no': "not wheelchair accessible",
+  'wheelchair_label_unknown': "unknown status",
+  'wheelchair_update_button': "Update",
+  'more_information': "more ...",
+  'wheelchair_help': "Wheelchair accessible? (Help)"
 };
-OpenLayers.Lang.es = { 
-  'wheelchair_label_yes': 'accesible en silla de ruedas',
-  'wheelchair_label_limited': 'acceso medio en silla de ruedas',
-  'wheelchair_label_no': 'sin acceso en silla de ruedas',
-  'wheelchair_label_unknown': 'se desconoce el estado',
-  'wheelchair_update_button': 'Guardar',
-  'more_information': 'más ...',
-  'wheelchair_help': '¿Acceso en silla de ruedas? (Ayuda)'
+OpenLayers.Lang.es = {
+  'wheelchair_label_yes': "accesible en silla de ruedas",
+  'wheelchair_label_limited': "acceso medio en silla de ruedas",
+  'wheelchair_label_no': "sin acceso en silla de ruedas",
+  'wheelchair_label_unknown': "se desconoce el estado",
+  'wheelchair_update_button': "Guardar",
+  'more_information': "más ...",
+  'wheelchair_help': "¿Acceso en silla de ruedas? (Ayuda)"
 };
-OpenLayers.Lang.fr = { 
-  'wheelchair_label_yes': 'accès pour fauteuil roulant',
-  'wheelchair_label_limited': 'accès partiel pour fauteuil roulant',
+OpenLayers.Lang.fr = {
+  'wheelchair_label_yes': "accès pour fauteuil roulant",
+  'wheelchair_label_limited': "accès partiel pour fauteuil roulant",
   'wheelchair_label_no': "pas d'accès pour fauteuil roulant",
-  'wheelchair_label_unknown': 'statut inconnu',
-  'wheelchair_update_button': 'Sauvegarder',
-  'more_information': 'davantage ...',
-  'wheelchair_help': 'Accessible pour fauteuil roulant? (Aide)'
+  'wheelchair_label_unknown': "statut inconnu",
+  'wheelchair_update_button': "Sauvegarder",
+  'more_information': "davantage ...",
+  'wheelchair_help': "Accessible pour fauteuil roulant? (Aide)"
 };
-OpenLayers.Lang.it = { 
-  'wheelchair_label_yes': 'accessibile in sedia a rotelle',
-  'wheelchair_label_limited': 'parzialmente accessibile in sedia a rotelle',
-  'wheelchair_label_no': 'non accessibile in sedia a rotelle',
-  'wheelchair_label_unknown': 'stato sconosciuto',
-  'wheelchair_update_button': 'Salva',
-  'more_information': 'più ...',
-  'wheelchair_help': 'Accessibile in sedia a rotelle? (Aiuto)'
+OpenLayers.Lang.is = {
+  'wheelchair_label_yes': "hjólastólaaðgengilegt",
+  'wheelchair_label_limited': "að hluta til hjólastólaaðgengilegt",
+  'wheelchair_label_no': "ekki hjólastólaaðgengilegt",
+  'wheelchair_label_unknown': "óvituð staða",
+  'wheelchair_update_button': "vista",
+  'more_information': "meiri ...",
+  'wheelchair_help': "Hjólastólaaðgengilegt? (Hjálp)"
 };
-OpenLayers.Lang.is = { 
-  'wheelchair_label_yes': 'hjólastólaaðgengilegt',
-  'wheelchair_label_limited': 'að hluta til hjólastólaaðgengilegt',
-  'wheelchair_label_no': 'ekki hjólastólaaðgengilegt',
-  'wheelchair_label_unknown': 'óvituð staða',
-  'wheelchair_update_button': 'vista',
-  'more_information': 'meiri ...',
-  'wheelchair_help': 'Hjólastólaaðgengilegt? (Hjálp)'
-  
+OpenLayers.Lang.it = {
+  'wheelchair_label_yes': "accessibile in sedia a rotelle",
+  'wheelchair_label_limited': "parzialmente accessibile in sedia a rotelle",
+  'wheelchair_label_no': "non accessibile in sedia a rotelle",
+  'wheelchair_label_unknown': "stato sconosciuto",
+  'wheelchair_update_button': "Salva",
+  'more_information': "più ...",
+  'wheelchair_help': "Accessibile in sedia a rotelle? (Aiuto)"
 };
-OpenLayers.Lang.jp = { 
-  'wheelchair_label_yes': '車椅子可',
-  'wheelchair_label_limited': '車椅子一部可',
-  'wheelchair_label_no': '車椅子不可',
-  'wheelchair_label_unknown': '不明',
-  'wheelchair_update_button': '保存',
-  'more_information': '詳細 ...',
-  'wheelchair_help': '車椅子で行けますか？ (ヘルプ)'
+OpenLayers.Lang.jp = {
+  'wheelchair_label_yes': "車椅子可",
+  'wheelchair_label_limited': "車椅子一部可",
+  'wheelchair_label_no': "車椅子不可",
+  'wheelchair_label_unknown': "不明",
+  'wheelchair_update_button': "保存",
+  'more_information': "詳細 ...",
+  'wheelchair_help': "車椅子で行けますか？ (ヘルプ)"
 };
-
-OpenLayers.Lang.ko = { 
- 'wheelchair_label_yes': '휠체어 접근가능',
- 'wheelchair_label_limited': '휠체어 부분적 접근가능',
- 'wheelchair_label_no': '휠체어 접근불가',
- 'wheelchair_label_unknown': '알 수 없는 상태',
- 'wheelchair_update_button': '업데이트',
- 'more_information': '자세히 ...',
- 'wheelchair_help': '휠체어 접근성? (도움말)'
+OpenLayers.Lang.ko = {
+  'wheelchair_label_yes': "휠체어 접근가능",
+  'wheelchair_label_limited': "휠체어 부분적 접근가능",
+  'wheelchair_label_no': "휠체어 접근불가",
+  'wheelchair_label_unknown': "알 수 없는 상태",
+  'wheelchair_update_button': "업데이트",
+  'more_information': "자세히 ...",
+  'wheelchair_help': "휠체어 접근성? (도움말)"
 };
-OpenLayers.Lang.tlh = { 
-  'wheelchair_label_yes': 'naw\'laH rutlhquS',
-  'wheelchair_label_limited': 'loQ naw\'laH rutlhquS',
-  'wheelchair_label_no': 'naw\'laHbe\' rutlhquS',
-  'wheelchair_label_unknown': 'Suvbe\'bogh Dotlh',
-  'wheelchair_update_button': 'qon',
-  'more_information': 'latlh ...',
-  'wheelchair_help': 'naw\'laH\'a\' rutlhquS? (QaH)'
+OpenLayers.Lang.se = {
+  'wheelchair_label_yes': "wheelchair accessible",
+  'wheelchair_label_limited': "partly wheelchair accessible",
+  'wheelchair_label_no': "not wheelchair accessible",
+  'wheelchair_label_unknown': "unknown status",
+  'wheelchair_update_button': "Update",
+  'more_information': "more ...",
+  'wheelchair_help': "Wheelchair accessible? (Help)"
 };
-OpenLayers.Lang.tr = { 
-  'wheelchair_label_yes': 'tekerlekli sandalye ile ulaşılır',
-  'wheelchair_label_limited': 'kısmen tekerlekli sandalye ile ulaşılır',
-  'wheelchair_label_no': 'tekerlekli sandalye için ulaşılır değil',
-  'wheelchair_label_unknown': 'belirsiz statü',
-  'wheelchair_update_button': 'Değişiklikeri kaydedin',
-  'more_information': 'daha fazla...',
-  'wheelchair_help': 'Tekerlekli sandalye için ulaşılırmı? (Yardım)' 
+OpenLayers.Lang.tlh = {
+  'wheelchair_label_yes': "naw'laH rutlhquS",
+  'wheelchair_label_limited': "loQ naw'laH rutlhquS",
+  'wheelchair_label_no': "naw'laHbe' rutlhquS",
+  'wheelchair_label_unknown': "Suvbe'bogh Dotlh",
+  'wheelchair_update_button': "qon",
+  'more_information': "latlh ...",
+  'wheelchair_help': "naw'laH'a' rutlhquS? (QaH)"
+};
+OpenLayers.Lang.tr = {
+  'wheelchair_label_yes': "tekerlekli sandalye ile ulaşılır",
+  'wheelchair_label_limited': "kısmen tekerlekli sandalye ile ulaşılır",
+  'wheelchair_label_no': "tekerlekli sandalye için ulaşılır değil",
+  'wheelchair_label_unknown': "belirsiz statü",
+  'wheelchair_update_button': "Değişiklikeri kaydedin",
+  'more_information': "daha fazla...",
+  'wheelchair_help': "Tekerlekli sandalye için ulaşılırmı? (Yardım)"
 };
