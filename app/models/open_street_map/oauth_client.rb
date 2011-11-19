d99a38c59914296d0f99dbcf4d53b14202672772
Christoph Bünte <info@christophbuente.de>
2011-11-19 01:15:02 +0100

    Add Content-Type header to OSM requests to fix communication.

--- app/models/open_street_map/oauth_client.rb
+++ app/models/open_street_map/oauth_client.rb
@@ -1,31 +1,31 @@
 class OpenStreetMap
   class OauthClient
-    
+
     attr_reader :access_token
-    
+
     def initialize(access_token)
       @access_token = access_token
     end
-    
+
     def put(url, options={})
       Rails.logger.debug("DELEGATED PUT!")
       Rails.logger.debug(url.inspect)
       Rails.logger.debug(options[:body].inspect)
       body = options[:body]
-      access_token.put(url, body)
+      access_token.put(url, body, {'Content-Type' => 'application/xml' })
     end
-    
+
     def delete(url, options={})
       Rails.logger.debug("DELEGATED DELETE!")
       Rails.logger.debug(url.inspect)
       Rails.logger.debug(options[:body].inspect)
       body = options[:body]
-      access_token.delete(url, body)
+      access_token.delete(url, body, {'Content-Type' => 'application/xml' })
     end
-    
+
     def post(*)
       Rails.logger.debug("DELEGATED POST!")
     end
-    
+
   end
 end
