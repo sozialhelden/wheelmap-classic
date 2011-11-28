5008ba87f289c34b7f5cf6a19499947a0666d847
Christoph Bünte <info@christophbuente.de>
2011-11-28 10:32:34 +0100

    Minor improvement in i18n locale switcher.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -17,7 +17,18 @@ set :rvm_bin_path, "/usr/local/rvm/bin"
 # role :app, "10.211.55.9"                          # This may be the same as your `Web` server
 # role :db,  "10.211.55.9", :primary => true # This is where Rails migrations will run
 # set :deploy_to, "/var/apps/wheelmap/staging"
-# 
+#
+# $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+# require "rvm/capistrano"
+# set :rvm_ruby_string, 'ree-1.8.7-2011.03'
+# set :rvm_type, :system
+# set :rvm_bin_path, "/usr/local/rvm/bin"
+# >>>>>>>>>>>>>>>>>>>>>> Virtual test server
+# role :web, "10.211.55.12"                          # Your HTTP server, Apache/etc
+# role :app, "10.211.55.12"                          # This may be the same as your `Web` server
+# role :db,  "10.211.55.12", :primary => true # This is where Rails migrations will run
+# set :deploy_to, "/srv/www/wheelmap/staging"
+#
 # $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 # require "rvm/capistrano"
 # set :rvm_ruby_string, 'ree-1.8.7-2011.03'
