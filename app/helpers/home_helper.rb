67fc26dc790a0ebbb1749c798c58bd81c16ca908
Christoph Bünte <info@christophbuente.de>
2011-12-01 20:45:18 +0100

    Prepare wheelmap for use of ruby 1.9.2

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,5 +1,6 @@
+# encoding: UTF-8
 module HomeHelper
-  
+
   def minimize_overlay(id)
     if cookies["minimized_#{id}"] == 'true'
       link_to '»', '#', :style => 'margin-top:-5px;float:right;display:block;', :class => 'maximize'
