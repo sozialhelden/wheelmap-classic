3dad5110f3a553c51f4e60f7a481169bb1a62c75
Christoph Bünte <info@christophbuente.de>
2011-12-05 12:20:17 +0100

    Add unicorn restart cap tasks.

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,6 +26,23 @@ after  'deploy:update_code',  'deploy:symlink_configs'
 after  'deploy:update_code',  'deploy:remove_all_unfinished_locales'
 after  'deploy',              'deploy:cache:clear'
 
+namespace :unicorn do
+  desc "Zero-downtime restart of Unicorn"
+  task :restart, :except => { :no_release => true } do
+    run "kill -s USR2 `cat #{shared_path}/pids/unicorn.pid`"
+  end
+
+  desc "Start unicorn"
+  task :start, :except => { :no_release => true } do
+    run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
+  end
+
+  desc "Stop unicorn"
+  task :stop, :except => { :no_release => true } do
+    run "kill -s QUIT `cat #{shared_path}/pids/unicorn.pid`"
+  end
+  after "deploy:restart", "unicorn:restart"
+end
 
 namespace :deploy do
   task :start do ; end
