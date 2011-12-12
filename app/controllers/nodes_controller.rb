a5c678facf1d96f8c99a0fbedf8c322a3d184c5f
Christoph Bünte <info@christophbuente.de>
2011-12-12 16:43:17 +0100

    Remove A/B tests

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -49,7 +49,6 @@ class NodesController < ApplicationController
     client = OpenStreetMap::OauthClient.new(user.access_token)
     Delayed::Job.enqueue(UpdateSingleAttributeJob.new(params[:id], user, client, :wheelchair => params[:wheelchair]))
     @node = Poi.find(params[:id])
-    bingo! "splash_screen" if session['_ab_tester']
     respond_to do |wants|
       wants.js{ render :json => {:message => t('nodes.update_wheelchair.successfull', :status => t("wheelchairstatus.#{params[:wheelchair]}"), :name => @node.headline), :wheelchair => params[:wheelchair] }.to_json}
       wants.html{ render :text => t('nodes.update_wheelchair.successfull') }
