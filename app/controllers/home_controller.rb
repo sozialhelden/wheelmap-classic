a641eb5e88b27ec8f14c14e77a1d9f03ed2b6be8
Christoph Bünte <info@christophbuente.de>
2011-12-12 16:43:17 +0100

    Remove A/B tests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   caches_action :index, :expires_in => 1.hour,
                         :unless => lambda { user_signed_in? || admin_signed_in? },
                         :cache_path => Proc.new {|c|
-                          c.params.delete_if { |k,v| %w{lat lon zoom q layers a}.include?(k) }.merge(:ab_splash_screen => ab_test("splash_screen").to_s)
+                          c.params.delete_if { |k,v| %w{lat lon zoom q layers a}.include?(k) }
                         }
 
   def index
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
 
   def after_sign_up
     flash[:notice] = nil
-    bingo! 'signup'
   end
 
   def after_sign_up_path_for(user)
