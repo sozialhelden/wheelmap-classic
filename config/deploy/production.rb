293ad89de84bb07de22cdf9b816de0aa35162d38
Christoph Bünte <info@christophbuente.de>
2011-12-12 16:54:56 +0100

    Adjust deploy settings for new server.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@ require "whenever/capistrano"
 set :whenever_command, "bundle exec whenever"
 set :whenever_environment, :production
 
-# Delayed Job  
+# Delayed Job
 require "delayed/recipes"
 before "deploy:restart", "delayed_job:stop"
 after  "deploy:restart", "delayed_job:start"
@@ -14,25 +14,22 @@ after "deploy:stop",  "delayed_job:stop"
 after "deploy:start", "delayed_job:start"
 
 # >>>>>>>>>>>>>>>>>>>>>> NEW SERVER
-role :web, "88.198.52.240"                          # Your HTTP server, Apache/etc
-role :app, "88.198.52.240"                          # This may be the same as your `Web` server
-role :db,  "88.198.52.240", :primary => true # This is where Rails migrations will run
+role :web, "176.9.63.170"                          # Your HTTP server, Apache/etc
+role :app, "176.9.63.170"                          # This may be the same as your `Web` server
+role :db,  "176.9.63.170", :primary => true        # This is where Rails migrations will run
 set :port, 22022
 set :deploy_to, "/var/apps/wheelmap/production"
 
-$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
-require "rvm/capistrano"
-set :rvm_ruby_string, 'ree-1.8.7-2011.03'
-set :rvm_type, :system
-set :rvm_bin_path, "/usr/local/rvm/bin"
+set :default_environment, {
+  'PATH' => '/opt/ruby-enterprise/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games'
+}
+
 # >>>>>>>>>>>>>>>>>>>>>> VIRTUAL SERVER
-# role :web, "10.211.55.9"                          # Your HTTP server, Apache/etc
-# role :app, "10.211.55.9"                          # This may be the same as your `Web` server
-# role :db,  "10.211.55.9", :primary => true # This is where Rails migrations will run
+# role :web, "10.211.55.12"                          # Your HTTP server, Apache/etc
+# role :app, "10.211.55.12"                          # This may be the same as your `Web` server
+# role :db,  "10.211.55.12", :primary => true # This is where Rails migrations will run
 # set :deploy_to, "/var/apps/wheelmap/production"
-# 
-# $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
-# require "rvm/capistrano"
-# set :rvm_ruby_string, 'ree-1.8.7-2011.03'
-# set :rvm_type, :system
-# set :rvm_bin_path, "/usr/local/rvm/bin"
+#
+# set :default_environment, {
+#   'PATH' => '/opt/ruby-enterprise/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games'
+# }
