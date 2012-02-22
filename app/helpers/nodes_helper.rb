a8e8447959fff476804a86ccf559ddaf968ad5b2
Enno Brehm <enno.brehm@gmail.com>
2012-02-22 13:33:36 +0100

    Add centralkey as tag.

--- app/helpers/nodes_helper.rb
+++ app/helpers/nodes_helper.rb
@@ -3,4 +3,14 @@ module NodesHelper
   def link_to_landing_page(link_name, region, status, type, options = {})
     link_to(link_name, url_for(:controller => 'landing_pages', :wheelchair => status, :node_type_id => type.identifier, :region_id => region.slug.name), options)
   end
+
+  def tag_line(node, key, options = nil)
+    if node.tags[key]
+      label = options.try(:[], :label_key) || t("formtastic.labels.#{key}")
+      content_tag(:tr) do
+        content_tag(:th, label || "formtastic.labels.#{key}") +
+        content_tag(:td, node.tags[key] || '&nbsp;')
+      end
+    end
+  end
 end
