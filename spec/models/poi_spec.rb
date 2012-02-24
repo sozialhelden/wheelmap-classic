364e61105ed06a4e3323144122ef24089df10d3b
Christoph Bünte <info@christophbuente.de>
2012-02-24 17:18:22 +0100

    WIP: validate type and tags properly.

--- spec/models/poi_spec.rb
+++ spec/models/poi_spec.rb
@@ -3,11 +3,24 @@ describe Poi do
 
   subject{ Factory.build(:poi)}
 
+  let! :node_type do
+    Factory.create(:node_type, :osm_key => :shop, :osm_value => :alcohol, :identifier => :alcohol)
+  end
+
   describe 'associations' do
   end
 
   describe 'validations' do
     it { should be_valid }
+
+    it "is not be valid without existing node type" do
+      subject.tags = {'name' => 'a_name', 'wheelchair' => 'yes'}
+      subject.type = :invalid
+      subject.should_not be_valid
+      NodeType.should_receive(:valid_combination?).with('shop', 'alcohol').at_least(:once).and_return(true)
+      subject.type = 'alcohol'
+      subject.should be_valid
+    end
   end
 
   # describe 'scopes' do
