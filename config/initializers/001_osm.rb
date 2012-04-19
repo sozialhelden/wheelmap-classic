26cc866c5a6d953049d8fee55892a5d21a12788a
Christoph Bünte <info@christophbuente.de>
2012-04-19 19:06:06 +0200

    wip: Remove old oauth connect and add test for omniauth.

--- config/initializers/001_osm.rb
+++ config/initializers/001_osm.rb
@@ -3,3 +3,4 @@ require 'yaml'
 osm_config = YAML.load_file("#{Rails.root}/config/open_street_map.yml")
 OpenStreetMapConfig = OpenStruct.new osm_config[Rails.env]
 Rosemary::Api.base_uri OpenStreetMapConfig.oauth_site
+
