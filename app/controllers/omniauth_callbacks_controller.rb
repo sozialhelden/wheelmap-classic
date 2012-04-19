6646fb2a8c487ca96762b97082ba60479e5b93c2
Christoph Bünte <info@christophbuente.de>
2012-04-19 13:12:25 +0200

    wip: Remove obsolete oauth fields from user.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,10 +3,16 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     osm_id = request.env['omniauth.auth']['uid']
     user = User.find_by_osm_id(osm_id)
     if user
+
       set_flash_message :notice, :success, :kind => 'OpenStreetMap'
       sign_in user
-      redirect_to root_url
+      raise "TODO save oauth data"
+      redirect_to root_url # maybe to somewhere else after_signin_url blah
     else
+      # OSM user not in wheelmap db
+      @user = User.create(:osm_id => osm_id, :oauth_token => 'sds')
+      raise "TODO save oauth data"
+      redirect_to edit_user_path()
       failure "OSM user not found"
     end
   end
