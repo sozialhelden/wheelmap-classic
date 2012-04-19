1353673672e929566c24ac6657b7452393b5a581
Christoph Bünte <info@christophbuente.de>
2012-04-19 13:16:05 +0200

    wip: Make user valid without password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,10 @@ class User < ActiveRecord::Base
     t.add :authentication_token, :as => :api_key
   end
 
+  def password_required?
+    false
+  end
+
   def app_authorized?
     oauth_authorized?
   end
