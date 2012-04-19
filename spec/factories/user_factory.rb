22029e29daa59d138f798b463f4241f7a19c4289
Christoph Bünte <info@christophbuente.de>
2012-04-19 13:16:05 +0200

    wip: Make user valid without password.

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -4,8 +4,8 @@ end
 
 Factory.define :user do |user|
   user.email                  { Factory.next :email }
-  user.password               'e5AGwTANzqKW'
-  user.password_confirmation  'e5AGwTANzqKW'
+  # user.password               'e5AGwTANzqKW'
+  # user.password_confirmation  'e5AGwTANzqKW'
   user.confirmed_at           { 10.days.ago }
 end
 
