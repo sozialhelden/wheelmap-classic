dfd622551823b7420f7ac12862654718509f269e
Christoph Bünte <info@christophbuente.de>
2012-04-19 19:06:06 +0200

    wip: Remove old oauth connect and add test for omniauth.

--- config/routes.rb
+++ config/routes.rb
@@ -8,22 +8,15 @@ Wheelmap::Application.routes.draw do
   root :to => 'home#index'
 
   devise_for :admins
-  devise_for :users, :controllers => { :sessions => 'sessions', :confirmations => 'confirmations', :omniauth_callbacks => 'omniauth_callbacks' } do
+  devise_for :users, :controllers => { :confirmations => 'confirmations', :omniauth_callbacks => 'omniauth_callbacks' } do
     get "/registrations/successful", :to => "registrations#after_sign_up"
   end
 
   resources :node_types, :only => :index
   resources :search, :only => :index
   resources :feeds, :only => :index
-  resources :oauth, :only => [:new, :index] do
-    collection do
-      get :revoke
-      get :callback
-      get :osm_register
-      get :authorize
-      get :grant
-      get :deny
-    end
+  resources :oauth, :only => [] do
+    get :register_osm, :on => :collection
   end
 
   resources :client_applications
