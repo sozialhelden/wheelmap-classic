f26be79960170c94972c2cd0f398da577d25234e
Christoph Bünte <info@christophbuente.de>
2012-04-20 18:36:17 +0200

    wip: fix and test omniauth callbacks.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,27 +1,39 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+
+  before_filter :check_for_valid_permissions, :only => :osm
+
   def osm
-    osm_id = request.env['omniauth.auth']['uid']
+    user = find_or_create_user
+    user.update_oauth_credentials(request.env['omniauth.auth']['credentials'])
 
-    return failure(t('devise.omniauth_callbacks.permission_missing')) unless osm_id
-
-    user = User.find_by_osm_id(osm_id)
-    if user
-      sign_in user
-      user.update_oauth_credentials(request.env['omniauth.auth']['credentials'])
+    if user.sign_in_count == 0
       set_flash_message :notice, :success, :kind => 'OpenStreetMap'
       redirect_to after_sign_in_path_for(:user)
     else
       # OSM user not in wheelmap db
-      user = User.create
-      sign_in user
-      user.update_attribute(:osm_id, osm_id)
-      user.update_oauth_credentials(request.env['omniauth.auth']['credentials'])
       redirect_to edit_user_path(user)
+    end
+    sign_in user
+  end
+
+  def failure
+    set_flash_message :alert, :failure, :kind => 'OpenStreetMap', :reason => (params[:message] || failure_message)
+    redirect_to after_omniauth_failure_path_for(:user)
+  end
+
+  private
+
+  def check_for_valid_permissions
+    osm_id = request.env['omniauth.auth']['uid']
+    unless osm_id
+      params[:message] = t('devise.omniauth_callbacks.permission_missing')
+      failure
     end
   end
 
-  def failure(message = nil)
-    set_flash_message :alert, :failure, :kind => 'OpenStreetMap', :reason => (message || failure_message)
-    redirect_to after_omniauth_failure_path_for(:user)
+  def find_or_create_user
+    User.find_or_create_by_osm_id(request.env['omniauth.auth']['uid'])
   end
+
 end
