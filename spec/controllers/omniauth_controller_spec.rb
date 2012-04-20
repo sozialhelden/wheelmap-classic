239aba1b73bbc94e0530b3fbaa07308b596620ff
Christoph Bünte <info@christophbuente.de>
2012-04-20 18:36:17 +0200

    wip: fix and test omniauth callbacks.

--- spec/controllers/omniauth_controller_spec.rb
+++ spec/controllers/omniauth_controller_spec.rb
@@ -5,14 +5,14 @@ describe OmniauthCallbacksController do
 
   before do
     OmniAuth.config.test_mode = true
-    OmniAuth.config.mock_auth[:osm] = {
+    OmniAuth.config.mock_auth[:osm] = OmniAuth::AuthHash.new(
        'provider' => 'osm',
        'uid' => '174',
        'credentials' => {
          'token' => 'token',
          'secret' => 'secret'
        }
-     }
+     )
     request.env["devise.mapping"] = Devise.mappings[:user]
     request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:osm]
   end
@@ -21,90 +21,82 @@ describe OmniauthCallbacksController do
     OmniAuth.config.test_mode = false
   end
 
-  context "known user" do
+  shared_examples "any authorized user" do
+
     let :user do
+      User.find_by_osm_id(174)
+    end
+
+    it "signs in the user" do
+      get :osm
+      controller.current_user.should eql user
+    end
+
+    it "updates the users' oauth credentials" do
+      get :osm
+      user.reload
+      user.oauth_token.should eql 'token'
+      user.oauth_secret.should eql 'secret'
+    end
+
+  end
+
+  context "known user" do
+
+    before do
       Factory.create(:user, :osm_id => 174)
     end
 
-    before do
-      User.should_receive(:find_by_osm_id).with("174").and_return(user)
+    it "does not create a new user" do
+      lambda {
+        get :osm
+      }.should_not change(User, :count)
     end
 
-    it "should update access token" do
-      user.should_receive(:update_oauth_credentials).with({
-        'token' => 'token',
-        'secret' => 'secret'
-      })
-      get :osm
-    end
+   it_behaves_like "any authorized user"
 
-    it "should sign in user" do
-      user.stub!(:update_oauth_credentials)
+    it "redirects to after_sign_in_path" do
       get :osm
-      controller.current_user.should == user
-    end
-
-    it "redirects" do
-      user.stub!(:update_oauth_credentials)
-      get :osm
-      response.should be_redirect
+      response.should redirect_to '/'
     end
   end
 
   context "new user" do
-    let :user do
-      Factory.build(:user)
-    end
 
     before do
-      User.should_receive(:find_by_osm_id).with("174").and_return(nil)
+      User.delete_all
     end
 
-    it "should create a user" do
-      User.should_receive(:create).and_return(user)
-      get :osm
+    it "does not create a new user" do
+      lambda {
+        get :osm
+      }.should change(User, :count).by(1)
     end
 
-    it "sets osm_id on new user" do
-      User.should_receive(:create).and_return(user)
-      get :osm
-      user.osm_id.should == 174
-      user.should_not be_changed # implies persisted
-    end
+    it_behaves_like "any authorized user"
 
-    it "sets oauth data on new user" do
-      User.should_receive(:create).and_return(user)
-      user.should_receive(:update_oauth_credentials).with({
-        'token' => 'token',
-        'secret' => 'secret'
-      })
-
+    it "redirects to after_sign_in_path" do
       get :osm
+      response.should redirect_to '/'
     end
   end
 
   context "failure" do
 
-    it "doesn't login user when no uid is provided via oauth" do
-      OmniAuth.config.mock_auth[:osm] = {
-      'provider' => 'osm',
-        'credentials' => {
-          'token' => 'token',
-          'secret' => 'secret'
-        }
-      }
-
+    before do
+      OmniAuth.config.mock_auth[:osm].delete('uid')
       request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:osm]
-      User.should_not_receive(:find_by_osm_id)
-      get :osm
-
-      controller.current_user.should be_nil
-      response.should redirect_to new_user_session_url
+      controller.should_not_receive(:find_or_create_user)
     end
 
-    it "should redirect to login page on failure" do
-      get :failure
-      response.should be_redirect user_sign_in_path
+    it "doesn't login user when no uid is provided via oauth" do
+      get :osm
+      controller.current_user.should be_nil
+    end
+
+    it "redirects user back to sign in page" do
+      get :osm
+      response.should redirect_to new_user_session_url
     end
   end
 end
