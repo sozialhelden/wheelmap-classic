a4dd5ce602ab6393136948161a49f08db804e254
Christoph Bünte <info@christophbuente.de>
2012-04-25 17:52:43 +0200

    wip: handle email confirmation on user edit and sign up.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,6 +27,62 @@ describe User do
     @user.oauth_secret.should be_nil
   end
 
+  context "confirmation" do
+
+    subject do
+      Factory.create(:user, :email => 'horst@wheelmap.de')
+    end
+
+    before do
+      ActionMailer::Base.deliveries.clear
+    end
+
+    it "should not send confirmation instructions on create" do
+      @user = Factory.create(:user, :email => 'horst@wheelmap.de')
+      ActionMailer::Base.deliveries.size.should eql 0
+    end
+
+    it "should send confirmation instructions after changing the email" do
+      subject.should_receive(:send_email_confirmation)
+      subject.update_attribute(:email, 'newemail@example.com')
+      subject.should_not be_confirmed
+    end
+
+    it "should not send confirmation instructions if email has not been changed" do
+      subject.should_not_receive(:send_email_confirmation)
+      subject.update_attribute(:first_name, 'Horst')
+    end
+
+    it "should not send confirmation instructions if email is blank" do
+      subject.should_receive(:send_email_confirmation)
+      subject.update_attribute(:email, nil)
+      ActionMailer::Base.deliveries.size.should eql 0
+      subject.should_not be_confirmed
+    end
+
+    it "should send an actual mail when calling send_email_confirmation and email set" do
+      subject.send_email_confirmation
+      ActionMailer::Base.deliveries.size.should eql 1
+    end
+
+    it "should not send an mail when email is missing" do
+      subject.email = nil
+      subject.send_email_confirmation
+      ActionMailer::Base.deliveries.size.should eql 0
+    end
+
+    it "should have subject line in confirmation email" do
+      subject.send_email_confirmation
+      ActionMailer::Base.deliveries.first.subject.should eql I18n.t('devise.mailer.confirmation_instructions.subject')
+    end
+
+    it "should send email to users email address" do
+      subject.email.should_not be_nil
+      subject.send_email_confirmation
+      ActionMailer::Base.deliveries.first.to.first.should eql subject.email
+    end
+  end
+
   context "authentication" do
 
     before(:each) do
