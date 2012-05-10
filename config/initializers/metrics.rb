68ec874c4f807807476dd4ed739c4eb3660748b8
Christoph Bünte <info@christophbuente.de>
2012-05-10 18:41:44 +0200

    Track all metrics with source set to hostname. Track wheelchair count after osm sync.

--- config/initializers/metrics.rb
+++ config/initializers/metrics.rb
@@ -5,7 +5,8 @@ metrics_config_file = Rails.root.join('config', 'metrics.yml')
 if !Rails.env.test? && File.exists?(metrics_config_file)
   metrics_config = YAML.load_file(metrics_config_file)[Rails.env]
   Librato::Metrics.authenticate(metrics_config['email'], metrics_config['api_key'])
-  LIBRATO_QUEUE = Librato::Metrics::Queue.new(:autosubmit_count => 600, :autosubmit_interval => 60)
+  hostname = `hostname`.gsub(/\n/, '')
+  LIBRATO_QUEUE = Librato::Metrics::Aggregator.new(:autosubmit_interval => 10, :source => hostname)
 else
   LIBRATO_QUEUE = nil
 end
