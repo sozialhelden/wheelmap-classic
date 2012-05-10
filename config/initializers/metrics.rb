929d05435de30d85bd060aa943114bf19672527b
Christoph Bünte <info@christophbuente.de>
2012-05-10 15:54:28 +0200

    Track response times with rails notifications to librato.

--- config/initializers/metrics.rb
+++ config/initializers/metrics.rb
@@ -5,4 +5,7 @@ metrics_config_file = Rails.root.join('config', 'metrics.yml')
 if !Rails.env.test? && File.exists?(metrics_config_file)
   metrics_config = YAML.load_file(metrics_config_file)[Rails.env]
   Librato::Metrics.authenticate(metrics_config['email'], metrics_config['api_key'])
+  LIBRATO_QUEUE = Librato::Metrics::Queue.new(:autosubmit_count => 100, :autosubmit_interval => 10)
+else
+  LIBRATO_QUEUE = nil
 end
---
+++ config/initializers/notifications.rb
@@ -0,0 +1,5 @@
+
+ActiveSupport::Notifications.subscribe "process_action.action_controller" do |name, start, finish, id, payload|
+  duration = (finish - start) * 1000
+  LIBRATO_QUEUE.add(:"requests_#{Rails.env}" => { :measure_time => start.to_i, :value => duration })
+end
