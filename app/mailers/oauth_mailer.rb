84b2477d033634596d2685100da13b8c3951c79b
Christoph Bünte <info@christophbuente.de>
2012-07-05 12:58:21 +0200

    Add mailer to send out reminders.

---
+++ app/mailers/oauth_mailer.rb
@@ -0,0 +1,23 @@
+class OauthMailer < ActionMailer::Base
+  default :from => "noreply@wheelmap.org"
+
+  def osm_reconnect(user)
+    @user = user
+    @url  = edit_user_url(user)
+    mail( :to => user.email,
+          :reply_to => "andi@wheelmap.org",
+          :subject => "Reminder: Konto zwischen Wheelmap.org und OpenStreetMap neu verbinden"
+        )
+  end
+
+
+  def osm_sign_up(user)
+    @user = user
+    @url  = edit_user_url(user)
+    mail( :to => user.email,
+          :reply_to => "andi@wheelmap.org",
+          :subject => "Jetzt die Wheelmap mit dem OpenStreetMap-Account nutzen"
+        )
+  end
+
+end
