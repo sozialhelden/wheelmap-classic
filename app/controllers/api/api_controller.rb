d3e30c74e494456edf75e2c0958b2f9e7d6ad679
Christoph Bünte <info@christophbuente.de>
2012-07-11 16:19:28 +0200

    Fix security issue, when api can be triggered without giving an api key.

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -116,7 +116,7 @@ class Api::ApiController < ApplicationController
   end
 
   def authenticate_by_token
-    @current_user = User.find_by_authentication_token(params[:api_key])
+    @current_user = User.find_by_authentication_token(params[:api_key]) if params[:api_key]
     render_exception_without_notification(Exception.new('invalid API key.'), 401) unless @current_user
   end
 end
