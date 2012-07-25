8a35f3c66e63a1f67ac19918a63ba8449a0e060f
Christoph Bünte <info@christophbuente.de>
2012-07-25 12:25:19 +0200

    Add rake task to generate bounding box from region.

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,14 +1,23 @@
 class Region < ActiveRecord::Base
   has_many :pois
-  
+
   has_friendly_id :name,
     :use_slug => true,
     :approximate_ascii => true,
     :ascii_approximation_options => :german,
     :strip_non_ascii => true
-  
+
 
   def to_polygon
-    "#{self.grenze.text_geometry_type} (#{self.grenze.text_representation(false)})"
+    self.grenze.as_wkt
+  end
+
+  # Array with bounding coordinates left, bottom, right, top
+  def bounding_box
+    geofactory = RGeo::Cartesian.factory
+    bounding_box = RGeo::Cartesian::BoundingBox.new(geofactory)
+    polygon = geofactory.parse_wkb(self.grenze.as_wkb)
+    bounding_box.add(polygon)
+    [bounding_box.max_x, bounding_box.min_y, bounding_box.min_x, bounding_box.max_y]
   end
 end
