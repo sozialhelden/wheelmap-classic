1afc8780924d12d3b8ad6e67ae79a257e584f526
Christoph Bünte <info@christophbuente.de>
2012-08-02 18:38:03 +0200

    Add rake task to report iphone stats.

--- app/models/iphone_counter.rb
+++ app/models/iphone_counter.rb
@@ -2,6 +2,10 @@ class IphoneCounter < ActiveRecord::Base
 
   validates :install_id, :uniqueness => true
 
+  def self.outdated
+    self.where(["created_at < ?", 7.days.ago])
+  end
+
   def self.unique_installs
     select('DISTINCT install_id').count
   end
@@ -9,7 +13,7 @@ class IphoneCounter < ActiveRecord::Base
   def self.device_versions
     versions = select('*, COUNT(id) AS counter').group('device_version')
     versions.inject({}) do |memo, v|
-      memo["device_version_#{v.device_version}"] = v.counter unless v.device_version.blank?
+      memo["device_version_#{normalize_key(v.device_version)}"] = v.counter unless v.device_version.blank?
       memo
     end
   end
@@ -17,7 +21,7 @@ class IphoneCounter < ActiveRecord::Base
   def self.os_versions
     versions = select('*, COUNT(id) AS counter').group('os_version')
     versions.inject({}) do |memo, v|
-      memo["os_version_#{v.os_version}"] = v.counter unless v.os_version.blank?
+      memo["os_version_#{normalize_key(v.os_version)}"] = v.counter unless v.os_version.blank?
       memo
     end
   end
@@ -25,8 +29,12 @@ class IphoneCounter < ActiveRecord::Base
   def self.app_versions
     versions = select('*, COUNT(id) AS counter').group('app_version')
     versions.inject({}) do |memo, v|
-      memo["app_version_#{v.app_version}"] = v.counter unless v.app_version.blank?
+      memo["app_version_#{normalize_key(v.app_version)}"] = v.counter unless v.app_version.blank?
       memo
     end
+  end
+
+  def self.normalize_key(key)
+    key.gsub(/[\s\-\.]+/, '_')
   end
 end
