d3c23226fd63b56843f5fadac91d2711caeb3d5a
Christoph Bünte <info@christophbuente.de>
2012-10-18 19:38:08 +0200

    Track usage of search functionality.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,23 +1,25 @@
 class SearchController < ApplicationController
-  
+
   URL = "http://nominatim.openstreetmap.org/search"
   # URL = "http://open.mapquestapi.com/nominatim/v1/search"
   DEFAULT_PARAMS = {:limit => 10, :osm_type => 'N', :email => 'info@wheelmap.org'}
   USERAGENT = "Wheelmap v1.0, (http://wheelmap.org)"
-  
+
   TIMEOUT = 1
-  
+
   before_filter :check_for_search_term
-  
+
   rescue_from Timeout::Error,     :with => :timeout
   rescue_from SocketError,        :with => :timeout
-  
+
   def index
     @search_url   = URI.parse(URL)
     @http = Net::HTTP.new(@search_url.host, @search_url.port)
     @http.read_timeout = 2
     @http.open_timeout = 2
     @query = DEFAULT_PARAMS.reverse_merge({:'accept-language' => I18n.locale, :q => params[:q]})
+    source = "search_#{(mobile_app? ? 'iphone' : 'website')}"
+    Counter.increment(source)
     respond_to do |wants|
       wants.js  {
         resp = make_request(:json)
@@ -39,7 +41,7 @@ class SearchController < ApplicationController
       }
     end
   end
-  
+
 private
 
   def make_request(format)
@@ -56,12 +58,12 @@ private
     end
     return resp
   end
-    
+
   def check_for_search_term
     if params[:q].blank?
       @message = I18n.t('nodes.errors.param_missing', :param => 'q')
       render :action => 'shared/error', :status => 406
     end
   end
-  
+
 end
