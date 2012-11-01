75ce6ceec6718c66b3005df3a2333eb4121b31b0
Christoph Bünte <info@christophbuente.de>
2012-11-01 16:21:10 +0100

    Edit user form in admin backend.

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register User do
 
+  scope :wants_newsletter
+
   index do
     column :first_name
     column :last_name
@@ -11,5 +13,18 @@ ActiveAdmin.register User do
     default_actions
   end
 
-  scope :wants_newsletter
+  form do |f|
+    f.inputs do
+      f.input :first_name
+      f.input :last_name
+      f.input :email, :input_html => { :readonly=>true }
+      f.input :wants_newsletter
+    end
+    f.inputs "OSM" do
+      f.input :osm_id, :label => 'OSM ID'
+      f.input :changeset_id
+      f.input :create_counter, :input_html => { :readonly=>true }
+      f.input :edit_counter, :input_html => { :readonly=>true }
+    end
+  end
 end
