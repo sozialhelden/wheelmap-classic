c7a2011c5c57a6d7cdb67686c613f375b0aac02b
Christoph Bünte <info@christophbuente.de>
2012-11-01 16:56:34 +0100

    Check if admin user is signed in for caching.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   caches_action :index, :expires_in => 1.hour,
-                        :unless => lambda { user_signed_in? || admin_signed_in? },
+                        :unless => lambda { user_signed_in? || admin_user_signed_in? },
                         :cache_path => Proc.new {|c|
                           c.params.delete_if { |k,v| %w{lat lon zoom q layers a}.include?(k) }
                         }
