28af4f171be63898176a4ca320b3ccaae61f84b3
Christoph Bünte <info@christophbuente.de>
2012-11-06 12:20:53 +0100

    Make osm username editable in admin backend.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
     :trackable, :validatable, :encryptable, :omniauthable, :encryptor => :sha1
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :wants_newsletter, :first_name, :last_name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :wants_newsletter, :first_name, :last_name, :osm_username
 
   validates :password, :confirmation =>true
 
