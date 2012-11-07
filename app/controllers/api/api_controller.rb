c5f29b4fd98f51c3a9226993d5cd78f5c102ce88
Christoph Bünte <info@christophbuente.de>
2012-11-07 16:56:49 +0100

    Fix sign in user via api.

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -117,7 +117,9 @@ class Api::ApiController < ApplicationController
 
   def authenticate_by_token
     api_key = params[:api_key] || request.headers["X-API-KEY"]
-    @current_user = sign_in(:user, User.find_by_authentication_token(api_key)) if api_key
-    render_exception_without_notification(Exception.new('invalid API key.'), 401) unless @current_user
+    if api_key && @current_user = User.find_by_authentication_token(api_key)
+      sign_in(:user, current_user)
+    end
+    render_exception_without_notification(Exception.new('invalid API key.'), 401) unless current_user
   end
 end
