3eac2dc822ec928809c34a52b2921d26d5409757
Christoph Bünte <info@christophbuente.de>
2012-11-09 10:25:09 +0100

    Use uploadify and lightbox to upload an slideshow.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,16 @@
 class Photo < ActiveRecord::Base
+  attr_accessible :image
   belongs_to :poi
-  mount_uploader :image, ImageUploader
+  belongs_to :user
+  mount_uploader :image, PhotoUploader
+  process_in_background :image
+
+  before_save :extract_date_time
+
+  protected
+
+  def extract_date_time
+    self.taken_at = EXIFR::JPEG.new(image.path).date_time rescue nil
+  end
+
 end
