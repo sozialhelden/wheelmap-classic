399525f23b9ef61c92070847f38b9ae4cdcf6297
Christoph Bünte <info@christophbuente.de>
2012-11-20 16:51:53 +0100

    Add photo resource to api.

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Photo do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  subject { Factory.build :photo }
+
+  it "should create a valid photo" do
+    subject.should be_valid
+  end
+
+  it "should extract date time at creation" do
+    subject.should_receive(:extract_date_time)
+    subject.save
+  end
+
+  context 'associations' do
+    it { should belong_to :user }
+    it { should belong_to :poi }
+  end
+
 end
