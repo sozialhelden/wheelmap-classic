41afb8f62ef6422508f64f5d2b6a8b128a495842
Christoph Bünte <info@christophbuente.de>
2012-11-21 18:00:15 +0100

    Add create and destroy actions for photos to api.

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -6,11 +6,38 @@ class Api::PhotosController < Api::ApiController
   actions :index, :create, :destroy
 
   defaults :resource_class => Photo, :collection_name => 'photos'
+
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
   def index
     @photos = collection
     respond_to do |format|
       format.xml      {render_for_api :simple, :xml  => @photos, :root => :photos, :meta => meta}
       format.json     {render_for_api :simple, :json => @photos, :root => :photos, :meta => meta}
+    end
+  end
+
+  def create
+    @photo = build_resource
+    @photo.user = current_user
+    create! do |success, failure|
+      success.json{ render :json => { :message  => 'OK'           }.to_json, :status => 201 }
+      failure.json{ render :json => { :error    => @photo.errors  }.to_json, :status => 400 }
+
+      success.xml{  render :xml  => { :message  => 'OK'           }.to_xml,  :status => 201 }
+      failure.xml{  render :xml  => { :error    => @photo.errors  }.to_xml,  :status => 400 }
+    end
+  end
+
+
+  def destroy
+    @photo = resource
+    destroy! do |success, failure|
+      success.json{ render :json => { :message  => 'OK'           }.to_json, :status => 200 }
+      failure.json{ render :json => { :error    => @photo.errors  }.to_json, :status => 400 }
+
+      success.xml{  render :xml  => { :message  => 'OK'           }.to_xml,  :status => 200 }
+      failure.xml{  render :xml  => { :error    => @photo.errors  }.to_xml,  :status => 400 }
     end
   end
 
@@ -26,6 +53,10 @@ class Api::PhotosController < Api::ApiController
 
   def collection
     @photos ||= parent.photos.paginate(:page => params[:page], :per_page => params[:per_page])
+  end
+
+  def resource
+    @photo ||= parent.photos.find(params[:id])
   end
 
 end
