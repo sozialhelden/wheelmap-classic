e3cc89db2a7c9441338413e7b324b39f6027297d
Christoph Bünte <info@christophbuente.de>
2012-11-27 12:25:28 +0100

    Fix photo view in admin backend.

--- app/admin/photos.rb
+++ app/admin/photos.rb
@@ -2,6 +2,12 @@ ActiveAdmin.register Photo do
   belongs_to :poi,  :optional => true
   belongs_to :user, :optional => true
 
+  controller do
+    def collection
+      super.with_user.with_poi # include user and poi
+    end
+  end
+
   index do
     column :id
     column :image do |photo|
