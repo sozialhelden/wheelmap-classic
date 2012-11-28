d0e52918d0f1fb0b74fb707dd8b564821dc0ee46
Christoph Bünte <info@christophbuente.de>
2012-11-28 15:43:24 +0100

    Use delayed job to process image resizing asynchronously.

--- db/migrate/20121128105658_add_width_and_height_to_photos.rb
+++ db/migrate/20121128105658_add_width_and_height_to_photos.rb
@@ -9,9 +9,15 @@ class AddWidthAndHeightToPhotos < ActiveRecord::Migration
         add_column :photos, :"image_#{version}_height", :integer
     end
 
+    add_column :photos, :image_processing, :boolean rescue nil
+    add_column :photos, :image_tmp, :string         rescue nil
+
   end
 
   def self.down
+    remove_column :photos, :image_processing rescue nil
+    remove_column :photos, :image_tmp        rescue nil
+
     [ :gallery_ipad_retina, :thumb, :thumb_iphone, :thumb_iphone_retina,
       :gallery_iphone, :gallery_iphone_retina, :gallery_ipad, :gallery].each do |version|
         remove_column :photos, :"image_#{version}_width"
