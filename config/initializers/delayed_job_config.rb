12b888893e5b3903f3f4535998b06952a431e972
Christoph Bünte <info@christophbuente.de>
2012-11-28 16:47:12 +0100

    Run background job every 5 seconds, do not run osm update jobs on staging.

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,6 +1,6 @@
 Delayed::Worker.destroy_failed_jobs = false
 # Delayed::Worker.destroy_successful_jobs = false
-Delayed::Worker.sleep_delay = 60
-Delayed::Worker.max_attempts = 25
+Delayed::Worker.sleep_delay = 5
+Delayed::Worker.max_attempts = 20
 Delayed::Worker.max_run_time = 5.minutes
 Delayed::Worker.logger = Logger.new("#{Rails.root}/log/delayed_job.log")
