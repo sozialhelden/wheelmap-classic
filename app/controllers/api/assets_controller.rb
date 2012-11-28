b7be27485865ed76bba48706f0f6eef1e20e29cd
Christoph Bünte <info@christophbuente.de>
2012-11-28 11:12:29 +0100

    Add assets resource to api and docs.

---
+++ app/controllers/api/assets_controller.rb
@@ -0,0 +1,34 @@
+class Api::AssetsController < Api::ApiController
+
+  actions :index
+  def index
+    index! do |format|
+      format.xml      {render_for_api :simple, :xml  => @assets, :root => :assets, :meta => meta}
+      format.json     {render_for_api :simple, :json => @assets, :root => :assets, :meta => meta}
+    end
+  end
+
+
+  protected
+
+  def collection
+    @assets ||= [Asset.new(:name => :marker), Asset.new(:name => :icons)]
+  end
+
+  def meta
+    @meta = {
+      :conditions => {
+        :page => 1,
+        :per_page => 15,
+        :format => params[:format],
+        :locale => params[:locale]
+      },
+      :meta => {
+        :page => 1,
+        :num_pages => 1,
+        :item_count_total => 2,
+        :item_count => 2
+      }
+    }
+  end
+end
