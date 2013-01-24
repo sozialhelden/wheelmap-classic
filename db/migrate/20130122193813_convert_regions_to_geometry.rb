8bf20f3bba0460a130e32802fef3f2e71d886931
Christoph Bünte <info@christophbuente.de>
2013-01-24 11:14:09 +0100

    Keep not null constrain on grenze column.

--- db/migrate/20130122193813_convert_regions_to_geometry.rb
+++ db/migrate/20130122193813_convert_regions_to_geometry.rb
@@ -2,13 +2,13 @@ class ConvertRegionsToGeometry < ActiveRecord::Migration
   def self.up
     change_table :regions do |t|
       #use general geometry to support polygons and multipolygons
-      t.change :grenze, :geometry
+      t.change :grenze, :geometry, :null => false
     end
   end
 
   def self.down
     change_table :regions do |t|
-      t.change :grenze, :polygon
+      t.change :grenze, :polygon, :null => false
     end
   end
 end
