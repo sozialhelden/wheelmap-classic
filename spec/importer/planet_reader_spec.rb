bc117d163269669b77c6a22affdcc97a4c4043d1
Christoph Bünte <info@christophbuente.de>
2013-01-29 14:51:45 +0100

    Do not import disused or abandoned places.

--- spec/importer/planet_reader_spec.rb
+++ spec/importer/planet_reader_spec.rb
@@ -31,6 +31,22 @@ PRIVATE_IMPORT = <<-XML
 </osm>
 XML
 
+ABANDONED_IMPORT = <<-XML
+<?xml version='1.0' encoding='UTF-8'?>
+<osm version="0.6" generator="pbf2osm">
+  <node id="20000000" lat="52.5065963" lon="13.3228529" version="7" changeset="5414080" user="miwie" uid="57526" timestamp="2010-08-06T06:47:01Z">
+    <tag k="disused" v="yes" />
+    <tag k="addr:city" v="Berlin" />
+    <tag k="addr:country" v="DE" />
+    <tag k="addr:postcode" v="10623" />
+    <tag k="amenity" v="restaurant" />
+    <tag k="cuisine" v="spanish" />
+    <tag k="name" v="Mar y Sol" />
+    <tag k="wheelchair" v="yes" />
+  </node>
+</osm>
+XML
+
 describe PlanetReader do
 
   before :each do
@@ -55,6 +71,11 @@ describe PlanetReader do
 
   it "should not import poi if access is private" do
     PlanetReader.new(StringIO.new(PRIVATE_IMPORT)).load
+    Poi.exists?(20000000).should be_false
+  end
+
+  it "should not import poi if disused or abandoned" do
+    PlanetReader.new(StringIO.new(ABANDONED_IMPORT)).load
     Poi.exists?(20000000).should be_false
   end
 end
