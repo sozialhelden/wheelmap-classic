412856bfce829a43d85463029a536df1798a61d7
Christoph Bünte <info@christophbuente.de>
2013-03-06 15:54:55 +0100

    Add better index names for every app and environment.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,10 @@ Spork.prefork do
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'factory_girl'
-  require 'webmock/rspec'
   require 'carrierwave/test/matchers'
+
+  require 'webmock'
+  WebMock.disable_net_connect!(:allow => "http://localhost:9200")
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
