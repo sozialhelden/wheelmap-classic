d0703d7a6ce512d03b47bcf9fcde9c289fad470b
Christoph Bünte <info@christophbuente.de>
2013-03-07 21:13:55 +0100

    Use curb for elastic search client.

--- config/initializers/tire.rb
+++ config/initializers/tire.rb
@@ -1,3 +1,6 @@
+require 'curb'
+require 'tire/http/clients/curb'
 Tire.configure do
   logger Rails.root.join("log", "tire_#{Rails.env}.log"), :level => Rails.env.development? ? 'debug' : 'info'
+  client Tire::HTTP::Client::Curb
 end
