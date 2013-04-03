b02525352b125cc61e54a82f4620116ff49eadc4
Simon Staib <simon.staib@netzpulsen.de>
2013-04-03 13:25:07 +0200

    Responsive Searchbar

--- public/javascripts/function.js
+++ public/javascripts/function.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 $('.btn-searchbar').click(function() {
-	$('.searchbar').toggleClass('active');
+	$('.navbar-form').toggleClass('active');
 });
 $('.notification.active').slideDown(500).delay(8000).slideUp(400, function() { $(this).remove()});
 
