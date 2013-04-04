2eb1c6b7d794f26dbd53d0d5a03d1cb6de9916c7
Simon Staib <simon.staib@netzpulsen.de>
2013-04-04 13:34:06 +0200

    Added Category Icon

--- public/javascripts/function.js
+++ public/javascripts/function.js
@@ -12,6 +12,21 @@ $('.close-btn, .btn-start').click(function() {
 	$('.overlay').fadeOut(500);
 });
 
+
+
+function ResizeElements() {
+	var width = $(window).width();
+	if (width < 420) {
+		$('.category-filter .filter-option').addClass('icon-tags');
+	}
+	else {
+		$('.category-filter .filter-option').removeClass('icon-tags');
+	}
+}
+$(window).load(ResizeElements);
+$(window).resize(ResizeElements);
+
+
 map.on('popupopen', function(e) {
   $('ol.wheelchair label, ol.wheelchair input').click(function() {
     $('li.save').fadeIn(1000);
