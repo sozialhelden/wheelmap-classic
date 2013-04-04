093425a1ff4482278ce7e8e7210ef13e725c6094
Christoph Bünte <info@christophbuente.de>
2013-04-04 15:48:34 +0200

    Hide url bar in ios browsers.

--- public/javascripts/function.js
+++ public/javascripts/function.js
@@ -1,27 +1,29 @@
 $(document).ready(function() {
+  $('.selectpicker').selectpicker();
+
+  I18n.defaultLocale = 'de';
+  I18n.locale = $('html').attr('lang');
 
   $('.status-filter button').popover({trigger: 'hover'});
 
-
   $('.btn-searchbar').click(function() {
-  	$('.navbar-form').toggleClass('active');
+    $('.navbar-form').toggleClass('active');
   });
   $('.notification.active').slideDown(500).delay(8000).slideUp(400, function() { $(this).remove()});
 
   $('.close-btn, .btn-start').click(function() {
-  	$('.overlay').fadeOut(500);
+    $('.overlay').fadeOut(500);
   });
 
 
-
   function ResizeElements() {
-  	var width = $(window).width();
-  	if (width < 420) {
-  		$('.category-filter .filter-option').addClass('icon-tags');
-  	}
-  	else {
-  		$('.category-filter .filter-option').removeClass('icon-tags');
-  	}
+    var width = $(window).width();
+    if (width < 420) {
+      $('.category-filter .filter-option').addClass('icon-tags');
+    }
+    else {
+      $('.category-filter .filter-option').removeClass('icon-tags');
+    }
   }
   $(window).load(ResizeElements);
   $(window).resize(ResizeElements);
@@ -32,12 +34,6 @@ $(document).ready(function() {
       $('li.save').fadeIn(1000);
     });
   });
-
-  $('.selectpicker').selectpicker();
-
-  I18n.defaultLocale = 'de';
-  I18n.locale = $('html').attr('lang');
-
 
   addEventListener("load", function() {
     new FastClick(document.body);
