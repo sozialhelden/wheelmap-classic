d5d783cd59cab6e266bab96d9c3fe34d945913e2
Christoph Bünte <info@christophbuente.de>
2013-04-18 20:34:56 +0200

    Offer kml download for regions.

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,2 +1,27 @@
 class RegionsController < ApplicationController
+  # Include Inherited Resources behaviour
+  inherit_resources
+
+  layout 'home'
+
+  has_scope :depth, :default => 1, :only => :index
+
+  # If no param is set, default to PER_PAGE
+  before_filter :set_default_depth
+
+  def index
+    @regions = collection
+  end
+
+  def show
+    @region = resource
+  end
+
+
+  protected
+
+  def set_default_depth
+    @depth ||= params[:depth] || 1
+  end
+
 end
