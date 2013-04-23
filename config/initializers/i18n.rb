b22b7d64f5cc3ebcf5a0044abe5bb8050ab79662
Christoph Bünte <info@christophbuente.de>
2013-04-23 12:46:05 +0200

    Use a fallback strategy for non existing languages.

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -2,6 +2,7 @@
 I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 I18n.default_locale = :de
 unless Rails.env.development?
+  I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
   I18n::Backend::Simple.send(:include, I18n::Backend::Cache)
   I18n.cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
 end
