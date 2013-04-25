3ed4048969eb1a5419e287d481c17573c904cb1a
Christoph Bünte <info@christophbuente.de>
2013-04-25 15:26:22 +0200

    Remove drs logo and add imprint link in footer [FIXES #48725423]

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -2,9 +2,9 @@ require 'compass'
 # If you have any compass plugins, require them here.
 Compass.add_project_configuration(File.join(Rails.root, "config", "compass.config"))
 Compass.configuration do |config|
-  config.project_path = RAILS_ROOT
+  config.project_path = Rails.root.to_s
+  config.environment = Rails.env.to_sym
   config.sass_dir = "app/stylesheets"
   config.css_dir = "public/stylesheets"
 end
-Compass.configuration.environment = Rails.env.to_sym
 Compass.configure_sass_plugin!
