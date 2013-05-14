c76e5ceb7e871dcb0476e07f69737972e9df4ec0
Simon Staib <simon.staib@netzpulsen.de>
2013-05-14 17:48:45 +0200

    Added Locate-Button

--- public/javascripts/leaflet.spin.js
+++ public/javascripts/leaflet.spin.js
@@ -23,18 +23,3 @@ L.SpinMapMixin = {
 };
 
 L.Map.include(L.SpinMapMixin);
-
-L.Map.addInitHook(function () {
-    this.on('layeradd', function (e) {
-        // If added layer is currently loading, spin !
-        if (e.layer.loading) this.spin(true);
-        e.layer.on('data:loading', function () { this.spin(true) }, this);
-        e.layer.on('data:loaded',  function () { this.spin(false) }, this);
-    }, this);
-    this.on('layerremove', function (e) {
-        // Clean-up
-        if (e.layer.loading) this.spin(false);
-        e.layer.off('data:loaded');
-        e.layer.off('data:loading');
-    }, this);
-});
