0f2ce36ee5181b54e733ab25eb7c7694b82c6efe
Enno Brehm <enno.brehm@immobilienscout24.de>
2013-06-19 15:14:42 +0200

    Don't create admin user automatically.

--- db/migrate/20121101140003_devise_create_admin_users.rb
+++ db/migrate/20121101140003_devise_create_admin_users.rb
@@ -15,9 +15,6 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    # Create a default user
-    AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password').confirm!
-
     add_index :admin_users, :email,                :unique => true
     add_index :admin_users, :reset_password_token, :unique => true
     add_index :admin_users, :confirmation_token,   :unique => true
