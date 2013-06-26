0a6d11d9f76203ac66362ffe69e3967fbeceb996
Christoph Bünte <info@christophbuente.de>
2013-06-26 16:35:23 +0200

    Remove last traces from queued nodes.

--- db/migrate/20130625150954_initial_schema.rb
+++ db/migrate/20130625150954_initial_schema.rb
@@ -191,16 +191,6 @@ class InitialSchema < ActiveRecord::Migration
       t.datetime "updated_at"
     end
 
-    create_table "queued_nodes", :force => true do |t|
-      t.integer  "user_id"
-      t.text     "node_attributes"
-      t.datetime "created_at"
-      t.datetime "updated_at"
-      t.point    "geom",            :limit => nil, :srid => 0
-    end
-
-    add_index "queued_nodes", ["geom"], :name => "index_queued_nodes_on_geom", :spatial => true
-
     create_table "regions", :force => true do |t|
       t.string   "name"
       t.datetime "created_at"
--- db/schema.rb
+++ db/schema.rb
@@ -204,16 +204,6 @@ ActiveRecord::Schema.define(:version => 20130625150954) do
     t.datetime "updated_at"
   end
 
-  create_table "queued_nodes", :force => true do |t|
-    t.integer  "user_id"
-    t.text     "node_attributes"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.spatial  "geom",            :limit => {:srid=>0, :type=>"point"}
-  end
-
-  add_index "queued_nodes", ["geom"], :name => "index_queued_nodes_on_geom", :spatial => true
-
   create_table "regions", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
