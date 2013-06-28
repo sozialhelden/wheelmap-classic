f4c0583f4bb665aed9bc82d63222dca99c14392e
Christoph Bünte <info@christophbuente.de>
2013-06-28 17:04:16 +0200

    Specify RGEO factory to be used for spatial column.

--- app/models/region.rb
+++ app/models/region.rb
@@ -14,6 +14,9 @@ class Region < ActiveRecord::Base
 
   attr_protected :lft, :rgt
 
+  GEO_FACTORY = RGeo::Cartesian.factory
+  set_rgeo_factory_for_column(:grenze, GEO_FACTORY)
+
   scope :parent_id, lambda {|parent_id| { :conditions => { :parent_id => parent_id }}}
   scope :depth, lambda { |depth| where(:depth => depth) }
 
