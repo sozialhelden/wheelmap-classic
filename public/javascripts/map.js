ca7b192b845a3ab0da3c2a13086f1c9552bf76c3
Christoph Bünte <info@christophbuente.de>
2013-07-23 15:14:56 +0200

    Reimplement search result page with new layout. [FIXES #50659847]

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -211,3 +211,68 @@ $('.category-filter').on('change', function(e){
     }
   })
 });
+
+var $statusFilterButtons = $('.status-filter button'),
+  $categoryFilter = $('.category-filter'),
+  filterCookie = false;
+
+$(['yes', 'no', 'limited', 'unknown']).each(function (index, item) {
+  if ($.cookie('filter_' + item)) {
+    filterCookie = true;
+    $statusFilterButtons.filter('.' + item).removeClass('active');
+  }
+});
+
+$categoryFilter.find('option').each(function () {
+  var filter_class = $(this).val();
+  if (!$.cookie('filter_' + filter_class)) {
+    $(this).attr('selected', 'selected');
+  }
+});
+
+$statusFilterButtons.popover({
+  trigger: 'hover',
+  placement: getPopoverPlacement
+}).click(function () {
+    $('.popover').fadeOut(300);
+  });
+
+$statusFilterButtons.click(function(event) {
+  if (filterCookie)
+    return;
+
+  filterCookie = true;
+  event.stopPropagation();
+  $statusFilterButtons.not(this).removeClass('active');
+  $statusFilterButtons.trigger('change');
+});
+
+$('.btn-searchbar').click(function () {
+  $('.navbar-form').toggleClass('active');
+});
+
+$('.notification.active').slideDown(500).delay(8000).slideUp(400, function () {
+  $(this).remove()
+});
+
+function getPopoverPlacement() {
+  if (window.innerWidth < 767) {
+    return 'top';
+  } else {
+    return 'bottom';
+  }
+}
+
+function ResizeElements() {
+  var widthTablet = $(window).width();
+  if (widthTablet < 767) {
+    $('.category-filter').addClass('dropup');
+  }
+  else {
+    $('.category-filter').removeClass('dropup');
+  }
+}
+
+$(window).load(ResizeElements);
+$(window).resize(ResizeElements);
+update_permalink('.createlink');
