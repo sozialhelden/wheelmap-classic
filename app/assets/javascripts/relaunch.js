467596e61ffa41da452db7efabc8686f757ecdc2
Christoph Bünte <info@christophbuente.de>
2013-07-24 14:48:05 +0200

    Upgrade to Rails3.1.

---
+++ app/assets/javascripts/relaunch.js
@@ -0,0 +1,33 @@
+// This is a manifest file that'll be compiled into including all the files listed below.
+// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
+// be included in the compiled file accessible from http://example.com/assets/application.js
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+//= require jquery
+//= require jquery_ujs
+//= require jquery/jquery.parsequery.js
+//= require jquery/jquery.cookies.js
+
+//
+//= require bootstrap-transition
+//= require bootstrap-button
+//= require bootstrap-collapse
+//= require bootstrap-dropdown
+//= require bootstrap-modal
+//= require bootstrap-tooltip
+//= require bootstrap-popover
+//= require bootstrap-select
+//
+//= require flash.js
+//= require i18n.js
+//= require translations.js
+//= require spin
+//
+//= require leaflet/leaflet
+//= require leaflet/leaflet.locate
+//= require leaflet/leaflet.spin
+//= require leaflet/leaflet.permalink
+//
+//= require map
+//= require function
