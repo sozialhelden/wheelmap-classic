1dd2d80aee552776d1476025a6c51cec27f11a32
Christoph Bünte <info@christophbuente.de>
2013-07-24 14:48:05 +0200

    Upgrade to Rails3.1.

---
+++ app/assets/javascripts/default.js
@@ -0,0 +1,23 @@
+// This is a manifest file that'll be compiled into including all the files listed below.
+// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
+// be included in the compiled file accessible from http://example.com/assets/application
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+//= require jquery
+//= require jquery_ujs
+//= require jquery/jquery-migrate-1.1.1
+//= require jquery/jquery.tools
+//= require jquery/jquery.blockUI
+//= require jquery/jquery.uploadify
+//= require jquery/jquery.lightbox
+//= require openlayers211
+//= require openlayers_i18n
+//= require osm
+//= require cookie
+//= require uservoice
+//= require json2
+//= require filter_settings
+//= require splash
+//= require flash
+//= require application
