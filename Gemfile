f3c4fe7e6eafc081b8a93207c27e888b03d38a85
Christoph Bünte <info@christophbuente.de>
2013-07-29 18:14:15 +0200

    Upgrade to Rails 3.2.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 group :default do
-  gem 'rails', '3.1.12'
+  gem 'rails', '3.2.14'
   gem 'mysql2'
   gem 'activerecord-import', '<0.3.0', :require => false
   gem 'rgeo'
--- db/structure.sql
+++ db/structure.sql
@@ -19,6 +19,7 @@ CREATE TABLE `admin_users` (
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `encrypted_password` varchar(128) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `reset_password_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `reset_password_sent_at` datetime DEFAULT NULL,
   `remember_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `remember_created_at` datetime DEFAULT NULL,
   `sign_in_count` int(11) DEFAULT '0',
@@ -35,7 +36,7 @@ CREATE TABLE `admin_users` (
   UNIQUE KEY `index_admin_users_on_email` (`email`),
   UNIQUE KEY `index_admin_users_on_reset_password_token` (`reset_password_token`),
   UNIQUE KEY `index_admin_users_on_confirmation_token` (`confirmation_token`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `alternatives` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -48,7 +49,7 @@ CREATE TABLE `alternatives` (
   PRIMARY KEY (`id`),
   KEY `index_alternatives_on_experiment_id` (`experiment_id`),
   KEY `index_alternatives_on_lookup` (`lookup`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `categories` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -76,7 +77,7 @@ CREATE TABLE `counters` (
   `search_iphone` int(11) DEFAULT '0',
   `search_android` int(11) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `delayed_jobs` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -88,15 +89,15 @@ CREATE TABLE `delayed_jobs` (
   `locked_at` datetime DEFAULT NULL,
   `failed_at` datetime DEFAULT NULL,
   `locked_by` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
   `first_started_at` datetime DEFAULT NULL,
   `last_started_at` datetime DEFAULT NULL,
   `finished_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
   `queue` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `delayed_jobs_priority` (`priority`,`run_at`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `experiments` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -106,7 +107,7 @@ CREATE TABLE `experiments` (
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `index_experiments_on_test_name` (`test_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `iphone_counters` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -117,7 +118,7 @@ CREATE TABLE `iphone_counters` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `node_types` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -133,14 +134,15 @@ CREATE TABLE `node_types` (
   PRIMARY KEY (`id`),
   KEY `index_node_types_on_id_and_category_id` (`id`,`category_id`),
   KEY `index_node_types_on_osm_key_and_osm_value` (`osm_key`,`osm_value`)
-) ENGINE=InnoDB AUTO_INCREMENT=425 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=113 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `photos` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `caption` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `image` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `image_processing` tinyint(1) DEFAULT NULL,
   `poi_id` bigint(20) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
   `taken_at` datetime DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `image_width` int(11) DEFAULT NULL,
@@ -161,29 +163,25 @@ CREATE TABLE `photos` (
   `image_gallery_ipad_height` int(11) DEFAULT NULL,
   `image_gallery_width` int(11) DEFAULT NULL,
   `image_gallery_height` int(11) DEFAULT NULL,
-  `image_processing` tinyint(1) DEFAULT NULL,
   `image_tmp` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=310 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `pois` (
   `osm_id` bigint(20) NOT NULL,
   `version` int(11) NOT NULL,
-  `tags` text COLLATE utf8_unicode_ci,
+  `tags` text COLLATE utf8_unicode_ci NOT NULL,
   `geom` point NOT NULL,
   `status` mediumint(9) NOT NULL DEFAULT '8',
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `node_type_id` bigint(20) DEFAULT NULL,
   `region_id` bigint(20) DEFAULT NULL,
-  `geoj` text COLLATE utf8_unicode_ci,
-  `geoj_dirty` tinyint(1) DEFAULT '1',
   UNIQUE KEY `index_pois_on_osm_id` (`osm_id`),
   SPATIAL KEY `index_pois_on_geom` (`geom`),
   KEY `index_pois_on_status` (`status`),
   KEY `index_pois_on_node_type_id_and_osm_id` (`node_type_id`,`osm_id`),
   KEY `index_pois_on_region_id` (`region_id`),
-  KEY `index_pois_on_region_id_and_status` (`region_id`,`status`),
   FULLTEXT KEY `fulltext_index_pois_on_tags` (`tags`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -197,7 +195,7 @@ CREATE TABLE `provided_pois` (
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `index_provided_pois_on_provider_id_and_poi_id` (`provider_id`,`poi_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=223 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `providers` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -205,7 +203,18 @@ CREATE TABLE `providers` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `queued_nodes` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `node_attributes` text COLLATE utf8_unicode_ci,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `geom` point NOT NULL,
+  PRIMARY KEY (`id`),
+  SPATIAL KEY `index_queued_nodes_on_geom` (`geom`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `regions` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -219,7 +228,7 @@ CREATE TABLE `regions` (
   `rgt` int(11) DEFAULT NULL,
   `depth` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=450 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `schema_migrations` (
   `version` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -237,97 +246,55 @@ CREATE TABLE `slugs` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `index_slugs_on_n_s_s_and_s` (`name`,`sluggable_type`,`sequence`,`scope`),
   KEY `index_slugs_on_sluggable_id` (`sluggable_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1277 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-CREATE TABLE `spatial_ref_sys` (
-  `SRID` int(11) NOT NULL,
-  `AUTH_NAME` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `AUTH_SRID` int(11) DEFAULT NULL,
-  `SRTEXT` varchar(2048) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `users` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `oauth_token` varchar(255) DEFAULT NULL,
-  `oauth_secret` varchar(255) DEFAULT NULL,
-  `email` varchar(255) NOT NULL DEFAULT '',
-  `encrypted_password` varchar(128) NOT NULL DEFAULT '',
-  `password_salt` varchar(255) NOT NULL DEFAULT '',
-  `remember_token` varchar(255) DEFAULT NULL,
+  `oauth_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `oauth_secret` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `encrypted_password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password_salt` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `remember_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `remember_created_at` datetime DEFAULT NULL,
   `sign_in_count` int(11) DEFAULT '0',
   `current_sign_in_at` datetime DEFAULT NULL,
   `last_sign_in_at` datetime DEFAULT NULL,
-  `current_sign_in_ip` varchar(255) DEFAULT NULL,
-  `last_sign_in_ip` varchar(255) DEFAULT NULL,
+  `current_sign_in_ip` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `last_sign_in_ip` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `changeset_id` bigint(20) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
-  `changeset_id` bigint(20) DEFAULT NULL,
+  `authentication_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `wants_newsletter` tinyint(1) NOT NULL DEFAULT '0',
-  `authentication_token` varchar(255) DEFAULT NULL,
-  `confirmation_token` varchar(255) DEFAULT NULL,
+  `confirmation_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `confirmed_at` datetime DEFAULT NULL,
   `confirmation_sent_at` datetime DEFAULT NULL,
   `osm_id` bigint(20) DEFAULT NULL,
-  `first_name` varchar(255) DEFAULT NULL,
-  `last_name` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `last_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `create_counter` int(11) NOT NULL DEFAULT '0',
   `edit_counter` int(11) NOT NULL DEFAULT '0',
-  `osm_username` varchar(255) DEFAULT NULL,
+  `osm_username` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `tag_counter` int(11) NOT NULL DEFAULT '0',
   `photos_count` int(11) NOT NULL DEFAULT '0',
   `terms` tinyint(1) NOT NULL DEFAULT '0',
   `accepted_at` datetime DEFAULT NULL,
-  `privacy_policy` tinyint(1) DEFAULT '0',
+  `privacy_policy` tinyint(1) NOT NULL DEFAULT '0',
   `privacy_policy_accepted_at` datetime DEFAULT NULL,
-  `reset_password_token` varchar(255) DEFAULT NULL,
+  `reset_password_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `reset_password_sent_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `index_users_on_email` (`email`),
   UNIQUE KEY `index_users_on_authentication_token` (`authentication_token`),
   KEY `index_users_on_oauth_token` (`oauth_token`),
   KEY `index_users_on_wants_newsletter` (`wants_newsletter`)
-) ENGINE=InnoDB AUTO_INCREMENT=11494 DEFAULT CHARSET=latin1;
-
-INSERT INTO schema_migrations (version) VALUES ('20100520103719');
-
-INSERT INTO schema_migrations (version) VALUES ('20100520103729');
-
-INSERT INTO schema_migrations (version) VALUES ('20100525074207');
-
-INSERT INTO schema_migrations (version) VALUES ('20100527092311');
-
-INSERT INTO schema_migrations (version) VALUES ('20100531150042');
-
-INSERT INTO schema_migrations (version) VALUES ('20100621072958');
-
-INSERT INTO schema_migrations (version) VALUES ('20100622123459');
-
-INSERT INTO schema_migrations (version) VALUES ('20100825052001');
-
-INSERT INTO schema_migrations (version) VALUES ('20100828215923');
-
-INSERT INTO schema_migrations (version) VALUES ('20100917130426');
-
-INSERT INTO schema_migrations (version) VALUES ('20101008093744');
-
-INSERT INTO schema_migrations (version) VALUES ('20101009153245');
-
-INSERT INTO schema_migrations (version) VALUES ('20101019161239');
-
-INSERT INTO schema_migrations (version) VALUES ('20101020065129');
-
-INSERT INTO schema_migrations (version) VALUES ('20101107090733');
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 INSERT INTO schema_migrations (version) VALUES ('20110107131649');
 
 INSERT INTO schema_migrations (version) VALUES ('20110114163727');
 
 INSERT INTO schema_migrations (version) VALUES ('20110427101005');
-
-INSERT INTO schema_migrations (version) VALUES ('20110503081429');
-
-INSERT INTO schema_migrations (version) VALUES ('20110503085046');
 
 INSERT INTO schema_migrations (version) VALUES ('20110504111051');
 
@@ -353,13 +320,9 @@ INSERT INTO schema_migrations (version) VALUES ('20111018102230');
 
 INSERT INTO schema_migrations (version) VALUES ('20111024103455');
 
-INSERT INTO schema_migrations (version) VALUES ('20111101112924');
-
 INSERT INTO schema_migrations (version) VALUES ('20111101122153');
 
 INSERT INTO schema_migrations (version) VALUES ('20111115120452');
-
-INSERT INTO schema_migrations (version) VALUES ('20111201194619');
 
 INSERT INTO schema_migrations (version) VALUES ('20120119150643');
 
@@ -368,8 +331,6 @@ INSERT INTO schema_migrations (version) VALUES ('20120120143510');
 INSERT INTO schema_migrations (version) VALUES ('20120202134608');
 
 INSERT INTO schema_migrations (version) VALUES ('20120203121324');
-
-INSERT INTO schema_migrations (version) VALUES ('20120217102914');
 
 INSERT INTO schema_migrations (version) VALUES ('20120413133645');
 
@@ -385,17 +346,11 @@ INSERT INTO schema_migrations (version) VALUES ('20120427103805');
 
 INSERT INTO schema_migrations (version) VALUES ('20120525115535');
 
-INSERT INTO schema_migrations (version) VALUES ('20120628101234');
-
 INSERT INTO schema_migrations (version) VALUES ('20120711130327');
-
-INSERT INTO schema_migrations (version) VALUES ('20120726081246');
 
 INSERT INTO schema_migrations (version) VALUES ('20120802105205');
 
 INSERT INTO schema_migrations (version) VALUES ('20121018171714');
-
-INSERT INTO schema_migrations (version) VALUES ('20121026103336');
 
 INSERT INTO schema_migrations (version) VALUES ('20121101130728');
 
@@ -425,14 +380,6 @@ INSERT INTO schema_migrations (version) VALUES ('20130117143229');
 
 INSERT INTO schema_migrations (version) VALUES ('20130122193813');
 
-INSERT INTO schema_migrations (version) VALUES ('20130205114724');
-
-INSERT INTO schema_migrations (version) VALUES ('20130205134253');
-
-INSERT INTO schema_migrations (version) VALUES ('20130205134630');
-
 INSERT INTO schema_migrations (version) VALUES ('20130214133154');
 
 INSERT INTO schema_migrations (version) VALUES ('20130228153529');
-
-INSERT INTO schema_migrations (version) VALUES ('20130521101713');
