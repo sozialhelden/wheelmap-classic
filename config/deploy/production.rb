e59fc77e27a95c36d242d097b841ae363a3ae4cb
Christoph Bünte <info@christophbuente.de>
2013-08-12 16:05:41 +0200

    Do not use ruby enterprise in path anymore.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,7 +15,6 @@ after  "delayed_job:start", "delayed_job:monitor"
 after "deploy:stop",  "delayed_job:stop"
 after "deploy:start", "delayed_job:start"
 
-# >>>>>>>>>>>>>>>>>>>>>> NEW SERVER
 role :web, "176.9.63.170"                          # Your HTTP server, Apache/etc
 role :app, "176.9.63.170"                          # This may be the same as your `Web` server
 role :db,  "176.9.63.170", :primary => true        # This is where Rails migrations will run
@@ -23,15 +22,5 @@ set :port, 22022
 set :deploy_to, "/var/apps/wheelmap/production"
 
 set :default_environment, {
-  'PATH' => '/opt/ruby-enterprise/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games'
+  'PATH' => '/opt/rbenv/shims:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games'
 }
-
-# >>>>>>>>>>>>>>>>>>>>>> VIRTUAL SERVER
-# role :web, "10.211.55.12"                          # Your HTTP server, Apache/etc
-# role :app, "10.211.55.12"                          # This may be the same as your `Web` server
-# role :db,  "10.211.55.12", :primary => true # This is where Rails migrations will run
-# set :deploy_to, "/var/apps/wheelmap/production"
-#
-# set :default_environment, {
-#   'PATH' => '/opt/ruby-enterprise/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games'
-# }
