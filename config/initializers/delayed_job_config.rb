9d0cdcae47aacf09901bae89727c31312026039c
Christoph Bünte <info@christophbuente.de>
2013-08-20 11:48:46 +0200

    Add simple librato measurement for delayed job.

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -4,3 +4,6 @@ Delayed::Worker.sleep_delay = 5
 Delayed::Worker.max_attempts = 20
 Delayed::Worker.max_run_time = 5.minutes
 Delayed::Worker.logger = Logger.new("#{Rails.root}/log/delayed_job.log")
+
+# require 'delayed_job_plugin'
+# Delayed::Worker.plugins << DelayedJobPlugin
