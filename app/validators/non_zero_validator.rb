8683804082e9eb8f4b4e59caad75abca3460248e
Christoph Bünte <info@christophbuente.de>
2013-10-01 14:09:43 +0200

    Do not allow user to create a poi at zero meridian. [FIXES: #57988754]

---
+++ app/validators/non_zero_validator.rb
@@ -0,0 +1,7 @@
+class NonZeroValidator < ActiveModel::EachValidator
+  def validate_each(object, attribute, value)
+    if value == 0
+      object.errors[attribute] << (options[:message] || I18n.t('activerecord.errors.messages.not_equal_to', count: 0.0))
+    end
+  end
+end
