fc71f0f446f8e14d023b7231b76f6933a892d904
Christoph Bünte <info@christophbuente.de>
2013-10-16 15:04:37 +0200

    Refactor poi model and put API stuff into a concern.

---
+++ app/representations/api/poi.rb
@@ -0,0 +1,67 @@
+module Api::Poi
+  extend ActiveSupport::Concern
+
+  included do
+
+    def around_api_response(api_template)
+      custom_cache_key = "api_response_#{self.cache_key}_#{api_template.to_s}"
+      Rails.cache.fetch(custom_cache_key, :expires_in => 1.day) do
+        yield
+      end
+    end
+
+    # Please do not edit, this is the current stable API JSON representation!!!
+    api_accessible :simple do |t|
+      t.add :name
+      t.add :wheelchair
+      t.add :wheelchair_description
+      t.add :node_type, :template => :id
+      t.add :lat
+      t.add :lon
+      t.add :id
+      t.add :category, :template => :id
+      t.add :street
+      t.add :housenumber
+      t.add :city
+      t.add :postcode
+      t.add :website
+      t.add :phone
+    end
+
+    api_accessible :osm do |t|
+      t.add lambda { |poi| poi.osm_id.abs }, :as => :id
+      t.add :lat
+      t.add :lon
+      t.add :tags, :as => :tag
+      t.add :version
+    end
+
+    # Please do not edit, this is the legacy API JSON for the first iphone version!!!
+    api_accessible :iphone do |t|
+      t.add :icon, :if => :icon
+      t.add :id
+      t.add :lat
+      t.add :lon
+      t.add :name
+      t.add :tags_without_blank_values, :as => :tags
+      t.add :type
+      t.add :wheelchair
+      t.add :category_for_node, :as => :category
+    end
+
+    api_accessible :map do |t|
+      t.add :id
+      t.add :name
+      t.add :lat
+      t.add :lon
+      t.add :breadcrumbs
+      t.add :address
+      t.add :wheelchair
+      t.add :accesibility
+      t.add lambda{|poi| poi.category.try(:identifier)  }, :as => :category
+      t.add lambda{|poi| poi.node_type.try(:identifier)  }, :as => :type
+      t.add :icon
+      t.add :osm_id
+    end
+  end
+end
