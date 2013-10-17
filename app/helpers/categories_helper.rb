a2d5dd9d0a8ccedc8ffe96d0b29d2424975a4425
Christoph Bünte <info@christophbuente.de>
2013-10-17 22:08:30 +0200

    Fix rendering JSON list in HTML.

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -4,6 +4,6 @@ module CategoriesHelper
   def render_json_list
     Category.all.map do |catgory|
       catgory.as_json(:only => [:id, :identifier])
-    end.join(',').as_json
+    end.to_json
   end
 end
