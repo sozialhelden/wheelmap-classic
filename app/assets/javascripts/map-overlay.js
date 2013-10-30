23576f4bc70fae7ecc2a458f13b6932028941339
lennerd <hello@lennerd.com>
2013-10-30 14:24:33 +0100

    Add overlay again and refactore css effects [FIXES #59822322].

---
+++ app/assets/javascripts/map-overlay.js
@@ -0,0 +1,24 @@
+(function($) {
+
+  if (!$.cookie('first_time_visitor')) {
+    $.cookie('first_time_visitor', true, { expires: 1000, path: '/' });
+
+    var $overlayWrapper = $('.overlay-wrapper');
+
+    $overlayWrapper.show();
+
+    setTimeout(function() {
+      $overlayWrapper.addClass('in');
+    }, 0);
+
+    $overlayWrapper.find('.close-btn, .go').click(function () {
+      if ($.support.transition.end) {
+        $overlayWrapper.one($.support.transition.end, function() {
+          $overlayWrapper.hide();
+        });
+      }
+      $overlayWrapper.removeClass('in');
+    });
+  }
+
+})(jQuery);
