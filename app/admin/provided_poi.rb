671742d354f6663c3840ce6058366034ac79483d
Christoph Bünte <info@christophbuente.de>
2013-11-12 15:35:47 +0100

    Speed up Premium list page filters.

--- app/admin/provided_poi.rb
+++ app/admin/provided_poi.rb
@@ -5,6 +5,11 @@ ActiveAdmin.register ProvidedPoi do
   belongs_to :provider, :optional => true
   belongs_to :poi, :optional => true
 
+  filter :provider, as: :select, collection: proc { Provider.all.inject([]){|memo,r| memo << [r.name, r.id]; memo}.sort}
+  filter :wheelchair, as: :select, collection: proc { Poi::WHEELCHAIR_STATUS_VALUES.map{|k,v| [t("wheelchairstatus.#{k}"),k]}}
+  filter :url
+
+
   index do
    column :id
    column :wheelchair do |p|
