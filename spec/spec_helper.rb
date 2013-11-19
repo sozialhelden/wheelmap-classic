c357fb9ba047058b556f3dcb9de5e6b2e08bbf88
Christoph Bünte <info@christophbuente.de>
2013-11-19 15:36:22 +0100

    Regenerate fixture and tag definitions. Do not load test gems in production.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,7 @@ Spork.prefork do
   require 'factory_girl'
   require 'webmock/rspec'
   require 'carrierwave/test/matchers'
+  require 'database_cleaner'
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
