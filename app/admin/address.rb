d336a5724e02075eb8d07568d2b1c3f98882b74a
Christoph Bünte <info@christophbuente.de>
2013-12-04 16:45:31 +0100

    Add geocodable adddresses

---
+++ app/admin/address.rb
@@ -0,0 +1,31 @@
+ActiveAdmin.register Address do
+
+  action_item :only => :index do
+    link_to 'Upload CSV', :action => 'upload_csv'
+  end
+
+  action_item :only => :index do
+    link_to 'Download KML', '/addresses.kml'
+  end
+
+
+  collection_action :upload_csv do
+    render "/admin/addresses/upload_csv"
+  end
+
+  collection_action :import_csv, :method => :post do
+    tmp_file = params[:address][:csv_file].read
+    Address.import(tmp_file)
+    redirect_to({:action => :index}, :notice => "CSV imported successfully!")
+  end
+
+  index do
+    selectable_column
+    column :coordinates do |a|
+      link_to "#{a.latitude}, #{a.longitude}", "http://wheelmap.org/map#/?lat=#{a.latitude}&lon=#{a.longitude}&zoom=18"
+    end
+    column :name
+    column :address
+    default_actions
+  end
+end
---
+++ app/models/address.rb
@@ -0,0 +1,20 @@
+# encoding: UTF-8
+
+class Address < ActiveRecord::Base
+  attr_accessible :latitude, :longitude, :name, :street, :zip, :city
+
+  geocoded_by :address
+
+  after_validation :geocode
+
+  def address
+    "#{street}, #{zip} #{city}"
+  end
+
+  def self.import(csv_file)
+    CSV.parse(csv_file, :headers => true, :encoding => 'LATIN-1', :header_converters => :symbol) do |row|
+      # raise row.inspect
+      Address.find_or_create_by_street_and_city_and_zip_and_name(row[:adresse], row[:ort], row[:plz], row[:name]) rescue nil
+    end
+  end
+end
