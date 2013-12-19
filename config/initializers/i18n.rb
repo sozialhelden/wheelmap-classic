d2d8c206118ab539c9ee6dccbbc41b6688e6abe7
Christoph Bünte <info@christophbuente.de>
2013-12-19 17:08:45 +0100

    Get rid of deprecation warnings.

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,4 +1,6 @@
 # # tell the I18n library where to find your translations
+I18n.enforce_available_locales = false
+
 I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 I18n.default_locale = :de
 if Rails.env.production?
