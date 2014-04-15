bec130e041bd95615e22732008021c767e17d032
lennerd <hello@lennerd.com>
2014-04-15 16:00:44 +0200

    Import existing translations to use them for new relaunch translation keys.

---
+++ app/assets/javascripts/i18n/ro_RO.js
@@ -0,0 +1,2 @@
+var I18n = I18n || {};
+I18n.translations = {"ro_RO":{"activerecord":{"attributes":{"poi":{"name":"Name","type":"Type","website":"Website","phone":"Phone","address_street":"Street","address_housenumber":"Housenumber","address_postcode":"Postcode","address_city":"City"}}}}};
