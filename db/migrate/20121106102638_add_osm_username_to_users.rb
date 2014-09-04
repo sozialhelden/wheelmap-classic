6b433bbd4423691892ffb47b31e38c2efeaa8752
Christoph Bünte <info@christophbuente.de>
2014-09-04 12:25:12 +0200

    Squash all migrations into a single one.

--- db/migrate/20121106102638_add_osm_username_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddOsmUsernameToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :osm_username, :string
-  end
-
-  def self.down
-    remove_column :users, :osm_username
-  end
-end
--- db/migrate/20131113134028_add_logo_to_providers.rb
+++
@@ -1,9 +0,0 @@
-class AddLogoToProviders < ActiveRecord::Migration
-  def up
-    add_column :providers, :logo, :string
-  end
-
-  def dow
-    remove_column :providers, :logo
-  end
-end
