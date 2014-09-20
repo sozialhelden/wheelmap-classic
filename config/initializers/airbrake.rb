cb1b2b2e93334833174e97ce1c60aee12b73ea6a
Christoph Bünte <info@christophbuente.de>
2014-09-20 13:30:57 +0200

    Bundle update airbrake.

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,7 @@
 Airbrake.configure do |config|
   config.api_key = ENV['airbrake_api_key']
+  config.ignore << "Rosemary::Conflict"
+  config.ignore << "Rosemary::ServerError"
+  config.ignore << "Net::ReadTimeout"
 end
 
