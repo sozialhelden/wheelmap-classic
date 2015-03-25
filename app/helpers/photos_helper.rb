d4663337ef7ad7cdd776fb825b26cb35fd2c4399
Christoph Bünte <info@christophbuente.de>
2015-03-25 17:43:34 +0100

    Put photo code into helper.

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,19 +1,27 @@
 module PhotosHelper
 
-  def photo_tag(photo, format = :thumb)
+  def photo_tag(photo, format = :thumb_iphone_retina)
     photo = Photo.new if photo.image_processing?
     image_tag(photo.image.url(format).to_s, :class => format,
                                             :alt => photo.id,
-                                            :width => photo.image.send(format).width,
-                                            :height => photo.image.send(format).height
+                                            :width => 160,
+                                            :height => 112,
+                                            :style  => "width:160px;height:112px;"
                                             )
   end
 
+  def delete_button(photo)
+    button_to("&times;".html_safe, profile_photo_path(photo), :confirm => t('devise.destroy.confirm'), :method => :delete, :class => [:delete, :btn, :'pull-right'], :title => t('devise.destroy.link'), :style => 'position:relative; top:-35px;left: -5px')
+  end
+
   def link_to_photo(photo)
+    html = ''
     photo = Photo.new if photo.image_processing?
-    link_name = image_path(photo.image.url(:gallery).to_s)
-    link_to link_name, :class => :thumb, :name => photo.id, :'data-lightbox' => 'gallery', :title => photo.caption do
-      photo_tag(photo)
+    link_name = image_path(photo.image.url(:thumb_iphone_retina).to_s)
+    html << link_to(link_name, :class => :thumbnail, :name => photo.id, :'data-lightbox' => 'gallery', :title => photo.caption) do
+      photo_tag(photo, :thumb_iphone_retina)
     end
+    html << delete_button(photo)
+    html.html_safe
   end
 end
