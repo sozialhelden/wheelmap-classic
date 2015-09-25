181545790f32375d26d725fbaf3aae15187107f6
Joost Oostdijk <joost.oostdijk@siili.fi>
2015-09-25 16:30:12 +0200

    Authenticate embed api with an api_key instead of the auth token

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,9 +1,9 @@
 class WidgetsController < ApplicationController
 
+  before_filter :authenticate_with_api_key
+
   def embed
-    api_key = params[:key] || request.headers["X-API-KEY"]
-    if user = User.find_by_authentication_token(api_key)
-        @widget = user.widget
+    if @widget
         render layout: 'embed'
     else
       head 404
@@ -26,4 +26,9 @@ class WidgetsController < ApplicationController
     params.require(:widget).permit(:lat, :lon, :height, :width, :categories)
   end
 
+  def authenticate_with_api_key
+    api_key = params[:key] || request.headers["X-API-KEY"]
+    user    = api_key && User.find_by_api_key(api_key)
+    @widget = user ? user.widget : nil
+  end
 end
