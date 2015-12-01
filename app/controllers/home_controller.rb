a2a2959d7f4cf3dda78f5a8ad2303a69464dfcbc
Lennart Hildebrandt <hello@lennerd.com>
2015-12-01 18:48:12 +0100

    Fix wrong usage of expires_in helper for home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,9 @@ class HomeController < ApplicationController
   layout 'relaunch'
 
   def index
-    expires_in 1.hour, public: true, unless: lambda { user_signed_in? || admin_user_signed_in? }
+    unless user_signed_in? || admin_user_signed_in?
+      expires_in 1.hour, public: true
+    end
 
     render
   end
