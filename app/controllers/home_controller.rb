85ccdedf5f13d15f58405a2bfed06e725de41b34
Lennart Hildebrandt <hello@lennerd.com>
2015-12-02 17:56:47 +0100

    Remove caching from home index action.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,13 @@ class HomeController < ApplicationController
   layout 'relaunch'
 
   def index
+=begin
+    Find a way to cache this:
+
     unless user_signed_in? || admin_user_signed_in?
       expires_in 1.hour, public: true
     end
+=end
 
     render
   end
