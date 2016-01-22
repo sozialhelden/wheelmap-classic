2f49851cdbd2a5dbfbf804d23a4701d49f14a8dc
Lennart Hildebrandt <hello@lennerd.com>
2016-01-22 11:09:16 +0100

    Add route action helpers.

--- gulp/reducers/common.router.js
+++ gulp/reducers/common.router.js
@@ -1,4 +1,7 @@
 const { createAction } = require('redux-actions');
+const each = require('lodash/collection/forEach');
+
+const Routes = require('../routes');
 
 const TRANSITION = 'TRANSITION';
 
@@ -6,11 +9,19 @@ function payloadCreatorCreator(method) {
   return (...args) => ({ method, args});
 }
 
-const methods = ['push', 'create', 'go', 'goForward', 'goBack'];
+const methods = ['push', 'replace', 'go', 'goForward', 'goBack'];
 const actions = {};
 
 methods.forEach(method => {
   actions[method] = createAction(TRANSITION, payloadCreatorCreator(method));
+});
+
+// Add action creators for pushing specific paths
+each(Routes, (method, name) => {
+  if (/Path$/i.test(name)) {
+    actions.push[name] = (...args) => actions.push(method.apply(Routes, args));
+    actions.replace[name] = (...args) => actions.replace(method.apply(Routes, args));
+  }
 });
 
 module.exports = {
