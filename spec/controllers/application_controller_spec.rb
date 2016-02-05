fdb35c998bf4bf5876bc79aa646ce166ecf04ce6
1000miles <thuy.le@asquera.de>
2016-02-05 15:22:45 +0000

    Add NoMethodError to remove raise_error warning after RSpec 3.4 Update

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -10,7 +10,7 @@ describe ApplicationController do
 
   it "should not include the rack_hotfix.rb initializer after rack 1.3.0" do
     if Rack.release.to_f >= 1.3
-      expect { Rack::Utils.hotfixed }.to raise_error
+      expect { Rack::Utils.hotfixed }.to raise_error(NoMethodError)
     end
   end
 end
