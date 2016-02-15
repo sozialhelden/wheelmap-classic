d53aa2a4ac20a6d36532645803fccaa09fdcc411
Jan Schulte <jan.schulte@asquera.de>
2016-02-15 13:33:45 +0000

    Add specs for LandingPagesController

---
+++ spec/controllers/landing_pages_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe LandingPagesController do
+  let(:region)    { FactoryGirl::create(:region) }
+  let(:node_type) { FactoryGirl::create(:node_type) }
+  describe '#index' do
+    before do
+      get :index, page: 1, region_id: region.id, wheelchair: :unknown, node_type_id: node_type.identifier
+    end
+
+    it 'returns http 200 status' do
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
