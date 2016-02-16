1c669fef25c731d2eb9f475d2afd620bd8dcdba9
Jan Schulte <jan.schulte@asquera.de>
2016-02-16 10:34:27 +0000

    Convert instance variables to local variables

--- app/controllers/node_types_controller.rb
+++ app/controllers/node_types_controller.rb
@@ -1,24 +1,24 @@
 class NodeTypesController < ApplicationController
   def index
     if params[:ids]
-      @node_types ||= NodeType.where(id: params[:ids])
+      node_types ||= NodeType.where(id: params[:ids])
     else
-      @node_types = NodeType.all
+      node_types = NodeType.all
     end
 
     respond_to do |format|
       format.json {
         render json: {
-            node_types: @node_types.as_api_response(:ember)
+            node_types: node_types.as_api_response(:ember)
         }
       }
     end
   end
 
   def show
-    @node_type = NodeType.find(params[:id])
+    node_type = NodeType.find(params[:id])
     respond_to do |format|
-      format.json { render json: { node_type: @node_type.as_api_response(:ember) }.to_json }
+      format.json { render json: { node_type: node_type.as_api_response(:ember) }.to_json }
     end
   end
 
