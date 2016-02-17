437adc9f0f8ac78c6a656a40ad9a0a29844d4e92
Lennart Hildebrandt <hello@lennerd.com>
2016-02-17 14:42:01 +0100

    Use proc for asset host protocoll.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,9 @@ Wheelmap::Application.configure do
   config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  config.action_controller.asset_host = "//asset%d.wheelmap.org"
+  config.action_controller.asset_host = Proc.new { |source, request|
+    "#{request.protocol}asset%d.wheelmap.org"
+  }
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
