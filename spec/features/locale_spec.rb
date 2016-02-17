1e774441ca0e62acb014a4b53cbbfd735f08a2d5
Jan Schulte <jan.schulte@asquera.de>
2016-02-17 09:20:04 +0000

    add specs for switching language manually

--- spec/features/locale_spec.rb
+++ spec/features/locale_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe "Locale feature" do
+  let(:languages) { "de;en;es;fr;it;is" }
   before do
-    languages = "de;en;es;fr;it;is"
     supported_languages = languages.split(';')
     I18n.available_locales = supported_languages
   end
@@ -14,7 +14,7 @@ describe "Locale feature" do
   describe "wheelmap in preferred language" do
     before do
       Capybara.current_session.driver.header("Accept-Language", languages)
-      visit root_path
+      visit '/map'
     end
 
     describe "Show wheelmap in preferred language" do
@@ -30,6 +30,38 @@ describe "Locale feature" do
 
       it "has locale de" do
         expect(I18n.locale).to eq :de
+      end
+    end
+
+    describe "Switch language manually to german" do
+      before do
+        Capybara.current_session.driver.header("Accept-Language", languages)
+        visit root_path
+      end
+      it 'has german locale' do
+        expect(I18n.locale).to eq :de
+      end
+    end
+
+    describe "Switch language manually to spanish" do
+      before do
+        Capybara.current_session.driver.header("Accept-Language", languages)
+        visit '/es'
+      end
+
+      it 'has spanish locale' do
+        expect(I18n.locale).to eq :es
+      end
+    end
+
+    describe "Switch language manually to english" do
+      before do
+        Capybara.current_session.driver.header("Accept-Language", languages)
+        visit '/en'
+      end
+
+      it 'has spanish locale' do
+        expect(I18n.locale).to eq :en
       end
     end
   end
