5df7a99da768aca3e93291263a4a2953dad4c915
Lennart Hildebrandt <hello@lennerd.com>
2016-02-19 10:02:06 +0100

    Save node after overview section.

--- config/initializers/jsroutes.rb
+++ config/initializers/jsroutes.rb
@@ -1,5 +1,5 @@
 JsRoutes.setup do |config|
-  config.include = [/^widgets$/, /^embed$/, /^new_node/, /^categories$/, /node$/]
+  config.include = [/^root$/, /^widgets$/, /^embed$/, /^new_node/, /^categories$/, /node$/, /^nodes$/]
   config.camel_case = true
   config.serializer = 'function(query) { return Wheelmap.stringifyQuery(query); }'
 end
