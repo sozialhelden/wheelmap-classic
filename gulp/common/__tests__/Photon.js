5552c8aa754686109912b97623886ce782c3e135
Lennart Hildebrandt <hello@lennerd.com>
2016-02-19 17:46:22 +0100

    Fix typo.

--- gulp/common/__tests__/Photon.js
+++ gulp/common/__tests__/Photon.js
@@ -1,10 +1,10 @@
-jest.dontMock('../photon');
+jest.dontMock('../Photon');
 
 const React = require('react');
 const ReactDOM = require('react-dom');
 const TestUtils = require('react-addons-test-utils');
 
-const Photon = require('../photon');
+const Photon = require('../Photon');
 
 describe("React Photon component", function() {
 
