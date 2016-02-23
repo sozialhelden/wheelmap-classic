f0a21c5b7fb55460fcd114246e5b120a1f516c73
Lennart Hildebrandt <hello@lennerd.com>
2016-02-23 18:27:59 +0100

    Add tests for navigateToNextSection saga.

--- gulp/NewNodeWidget/sagas/__tests__/initNode.js
+++ gulp/NewNodeWidget/sagas/__tests__/initNode.js
@@ -11,31 +11,26 @@ import api from '../../../common/helpers/api';
 const initNode = require('../initNode').default;
 
 describe('initNode', () => {
-  beforeEach(() => {
-    changeNode.mockClear();
-    api.validateNode.mockClear();
-  });
-
   it('redirects to name & category section if no node in query', () => {
-    const generator = initNode();
+    const gen = initNode();
 
-    expect(generator.next().value)
+    expectNext(gen).value
       .toEqual(take(ENTER_CONTENT));
 
     const nextState = { params: {}, location: { query: {} } },
       action = enterContent(nextState);
 
-    expect(generator.next(action).value)
+    expectNext(gen, action).value
       .toEqual(put(replace.newNodeSectionPath(NAME_CATEGORY)));
 
-    expect(generator.next().done)
+    expectNext(gen).done
       .toBe(true);
   });
 
   it('initializes node', () => {
-    const generator = initNode();
+    const gen = initNode();
 
-    expect(generator.next().value)
+    expectNext(gen).value
       .toEqual(take(ENTER_CONTENT));
 
     const queryNode = { lat: '15.5', lon: '13.3', title: 'Test Node' },
@@ -48,37 +43,37 @@ describe('initNode', () => {
     node.lon = 13.3;
     node.title = 'Test Node';
 
-    expect(generator.next(action).value)
+    expectNext(gen, action).value
       .toEqual(call(Node.create, { lat: node.lat, lon: node.lon, title: node.title }));
 
-    expect(generator.next(node).value)
+    expectNext(gen, node).value
       .toEqual(put(changeNode(node)));
 
-    expect(generator.next().value)
+    expectNext(gen).value
       .toEqual(call([node, node.location]));
 
     const location = { lat: node.lat, lon: node.lon };
 
-    expect(generator.next(location).value)
+    expectNext(gen, location).value
       .toEqual(put(changeMapCenter(location)));
 
-    expect(generator.next().value)
+    expectNext(gen).value
       .toEqual(put(load(true)));
 
-    expect(generator.next().value)
+    expectNext(gen).value
       .toEqual(call(api.validateNode, node));
 
-    expect(generator.next().value)
+    expectNext(gen).value
       .toEqual(put(load(false)));
 
-    expect(generator.next().done)
+    expectNext(gen).done
       .toBe(true);
   });
 
   it('redirects to name & category section if node is invalid', () => {
-    const generator = initNode();
+    const gen = initNode();
 
-    expect(generator.next().value)
+    expectNext(gen).value
       .toEqual(take(ENTER_CONTENT));
 
     const queryNode = { lat: '15.5', lon: '13.3', title: 'Test Node' },
@@ -91,39 +86,39 @@ describe('initNode', () => {
     node.lon = 13.3;
     node.title = 'Test Node';
 
-    expect(generator.next(action).value)
+    expectNext(gen, action).value
       .toEqual(call(Node.create, { lat: node.lat, lon: node.lon, title: node.title }));
 
-    expect(generator.next(node).value)
+    expectNext(gen, node).value
       .toEqual(put(changeNode(node)));
 
-    expect(generator.next().value)
+    expectNext(gen).value
       .toEqual(call([node, node.location]));
 
     const location = { lat: node.lat, lon: node.lon };
 
-    expect(generator.next(location).value)
+    expectNext(gen, location).value
       .toEqual(put(changeMapCenter(location)));
 
-    expect(generator.next().value)
+    expectNext(gen).value
       .toEqual(put(load(true)));
 
-    expect(generator.next().value)
+    expectNext(gen).value
       .toEqual(call(api.validateNode, node));
 
     const validationError = new api.HTTPError();
     validationError.errors = { error: 'error message' };
 
-    expect(generator.throw(validationError).value)
+    expectThrow(gen, validationError).value
       .toEqual(put(replace.newNodeSectionPath(NAME_CATEGORY)));
 
-    expect(generator.next().value)
+    expectNext(gen).value
       .toEqual(put(setErrors(validationError.errors)));
 
-    expect(generator.next().value)
+    expectNext(gen).value
       .toEqual(put(load(false)));
 
-    expect(generator.next().done)
+    expectNext(gen).done
       .toBe(true);
   });
 });
