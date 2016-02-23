014a0a13621ce604b81da340cb064e10d87bee96
Lennart Hildebrandt <hello@lennerd.com>
2016-02-23 18:36:55 +0100

    Refactore helper to test generators.

--- gulp/NewNodeWidget/sagas/__tests__/initNode.js
+++ gulp/NewNodeWidget/sagas/__tests__/initNode.js
@@ -12,25 +12,25 @@ const initNode = require('../initNode').default;
 
 describe('initNode', () => {
   it('redirects to name & category section if no node in query', () => {
-    const gen = initNode();
+    const gen = expectGen(initNode());
 
-    expectNext(gen).value
+    gen.next().value
       .toEqual(take(ENTER_CONTENT));
 
     const nextState = { params: {}, location: { query: {} } },
       action = enterContent(nextState);
 
-    expectNext(gen, action).value
+    gen.next(action).value
       .toEqual(put(replace.newNodeSectionPath(NAME_CATEGORY)));
 
-    expectNext(gen).done
+    gen.next().done
       .toBe(true);
   });
 
   it('initializes node', () => {
-    const gen = initNode();
+    const gen = expectGen(initNode());
 
-    expectNext(gen).value
+    gen.next().value
       .toEqual(take(ENTER_CONTENT));
 
     const queryNode = { lat: '15.5', lon: '13.3', title: 'Test Node' },
@@ -43,37 +43,37 @@ describe('initNode', () => {
     node.lon = 13.3;
     node.title = 'Test Node';
 
-    expectNext(gen, action).value
+    gen.next(action).value
       .toEqual(call(Node.create, { lat: node.lat, lon: node.lon, title: node.title }));
 
-    expectNext(gen, node).value
+    gen.next(node).value
       .toEqual(put(changeNode(node)));
 
-    expectNext(gen).value
+    gen.next().value
       .toEqual(call([node, node.location]));
 
     const location = { lat: node.lat, lon: node.lon };
 
-    expectNext(gen, location).value
+    gen.next(location).value
       .toEqual(put(changeMapCenter(location)));
 
-    expectNext(gen).value
+    gen.next().value
       .toEqual(put(load(true)));
 
-    expectNext(gen).value
+    gen.next().value
       .toEqual(call(api.validateNode, node));
 
-    expectNext(gen).value
+    gen.next().value
       .toEqual(put(load(false)));
 
-    expectNext(gen).done
+    gen.next().done
       .toBe(true);
   });
 
   it('redirects to name & category section if node is invalid', () => {
-    const gen = initNode();
+    const gen = expectGen(initNode());
 
-    expectNext(gen).value
+    gen.next().value
       .toEqual(take(ENTER_CONTENT));
 
     const queryNode = { lat: '15.5', lon: '13.3', title: 'Test Node' },
@@ -86,39 +86,39 @@ describe('initNode', () => {
     node.lon = 13.3;
     node.title = 'Test Node';
 
-    expectNext(gen, action).value
+    gen.next(action).value
       .toEqual(call(Node.create, { lat: node.lat, lon: node.lon, title: node.title }));
 
-    expectNext(gen, node).value
+    gen.next(node).value
       .toEqual(put(changeNode(node)));
 
-    expectNext(gen).value
+    gen.next().value
       .toEqual(call([node, node.location]));
 
     const location = { lat: node.lat, lon: node.lon };
 
-    expectNext(gen, location).value
+    gen.next(location).value
       .toEqual(put(changeMapCenter(location)));
 
-    expectNext(gen).value
+    gen.next().value
       .toEqual(put(load(true)));
 
-    expectNext(gen).value
+    gen.next().value
       .toEqual(call(api.validateNode, node));
 
     const validationError = new api.HTTPError();
     validationError.errors = { error: 'error message' };
 
-    expectThrow(gen, validationError).value
+    gen.throw(validationError).value
       .toEqual(put(replace.newNodeSectionPath(NAME_CATEGORY)));
 
-    expectNext(gen).value
+    gen.next().value
       .toEqual(put(setErrors(validationError.errors)));
 
-    expectNext(gen).value
+    gen.next().value
       .toEqual(put(load(false)));
 
-    expectNext(gen).done
+    gen.next().done
       .toBe(true);
   });
 });
