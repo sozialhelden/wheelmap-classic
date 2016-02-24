f2e90baa89842998dfee9d59b5bcccb2cebf9426
Lennart Hildebrandt <hello@lennerd.com>
2016-02-24 18:37:44 +0100

    Fix path helper to use the current locale.

--- gulp/common/routes.js
+++ gulp/common/routes.js
@@ -1,1 +1,20 @@
-module.exports = global.Routes;
+const map = require('mout/object/map');
+
+let routes = global.Routes;
+
+// Wrap route helper if main locale differs from current locale
+if (I18n.mainLocale !== I18n.locale) {
+  routes = map(routes, route => {
+    const routeWrapper = (...args) => {
+      return `/${I18n.locale}${route(...args)}`;
+    };
+
+    routeWrapper.toString = () => {
+      return `/${I18n.locale}${route.toString()}`;
+    };
+
+    return routeWrapper;
+  });
+}
+
+module.exports = routes;
