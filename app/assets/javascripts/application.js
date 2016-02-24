e6ecff09dd7cfb9392793775bdcd676ffb430f13
Lennart Hildebrandt <hello@lennerd.com>
2016-02-24 18:37:44 +0100

    Fix path helper to use the current locale.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,5 +5,6 @@
 //= require dist/components
 
 I18n.defaultLocale = 'en';
+I18n.mainLocale = 'de';
 I18n.locale = document.getElementsByTagName('html')[0].getAttribute('lang');
 I18n.fallbacks = true;
