47c384740b50c37120a1b9fd06bc3dc4621eaf4e
1000miles <thuy.le@asquera.de>
2016-02-25 16:01:12 +0000

    Refactor nodes-feature

--- spec/features/nodes_spec.rb
+++ spec/features/nodes_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "Nodes Feature" do
 
-  describe 'Find, update and create nodes via API' do
+  describe "Find, update and create nodes via API" do
 
     before do
       # Given a provider "stiftung" exists with name: "Stiftung Gesundheit"
@@ -16,41 +16,55 @@ describe "Nodes Feature" do
     end
 
     # Scenario: I want to see a decent title on the poi detail page
-    it "has a decent title on the poi detail page" do
-        # Then I should see "Dieser Ort ist auf Wheelmap.org: name, Bar, Essen & Trinken, Berlin" within "title"
-        expect(page.title).to eq("Dieser Ort ist auf Wheelmap.org: name, Bar, Essen & Trinken, Berlin")
-    end
+    describe "poi detail page" do
+      it "has a decent title" do
+          # Then I should see "Dieser Ort ist auf Wheelmap.org: name, Bar, Essen & Trinken, Berlin" within "title"
+          title_text = "Dieser Ort ist auf Wheelmap.org: name, Bar, Essen & Trinken, Berlin"
+          expect(page.title).to eq(title_text)
+      end
 
-    # Scenario: I want to see a link to a landing page on the poi detail page
-    it "has a link to a landing page on the poi detail page" do
-      # Then I should see "Alle Orte vom Typ"
-      expect(page).to have_link('Alle Orte vom Typ', href: "/map/berlin/bar/wheelchair/yes")
-    end
+      # Scenario: I want to see a link to a landing page on the poi detail page
+      it "has a link to a landing page" do
+        # Then I should see "Alle Orte vom Typ"
+        expect(page).to have_link('Alle Orte vom Typ', href: "/map/berlin/bar/wheelchair/yes")
+      end
 
-    # Scenario: I want to see a back link to the root page
-    it "has a back link to the root page" do
-      save_page
-      # When I follow "zurück"
-      click_link('zurück')
-      # Then I should be on the home page
-      expect(current_path).to eq(root_path)
+      # Scenario: I want to see a back link to the root page
+      it "has a back link to the root page" do
+        # When I follow "zurück"
+        click_link('zurück')
+        # Then I should be on the home page
+        expect(current_path).to eq(root_path)
+      end
     end
 
     # Scenario: I want to see a link to provider's detail page
-    # Then I should see "Stiftung Gesundheit"
-    it "shows 'Stiftung Gesundheit' on provider's detail page" do
-      expect(page).to have_content("Stiftung Gesundheit")
-    end
+    describe "provider detail page (e.g. Stiftung Gesundheit)" do
+      # Then I should see "Stiftung Gesundheit"
+      it "has info box content: 'Stiftung Gesundheit'" do
+        expect(page).to have_content("Stiftung Gesundheit")
+      end
 
-    # And I should see "Voll Rollstuhlgerecht"
-    it "shows 'voll rollstuhlgerecht' on 'Stiftung Gesundheit' detail page" do
-      expect(page).to have_content("Voll Rollstuhlgerecht")
-    end
+      describe 'dropdown' do
+        # And I should see "Voll Rollstuhlgerecht"
+        it "has content 'voll rollstuhlgerecht'" do
+          expect(page).to have_content("Voll Rollstuhlgerecht")
+        end
 
-    # Scenario: I want to see a wheelchair description
-    it "has a wheelchair description" do
-      # Then I should see "Yes, we have a ramp."
-      expect(page).to have_content("Yes, we have a ramp.")
+        it "has content 'teilweise rollstuhlgerecht'" do
+          expect(page).to have_content("Teilweise rollstuhlgerecht")
+        end
+
+        it "has content 'nicht rollstuhlgerecht'" do
+          expect(page).to have_content("Nicht Rollstuhlgerecht")
+        end
+      end
+
+      # Scenario: I want to see a wheelchair description
+      it "has a wheelchair description" do
+        # Then I should see "Yes, we have a ramp."
+        expect(page).to have_content("Yes, we have a ramp.")
+      end
     end
   end
 end
