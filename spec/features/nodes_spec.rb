c9cbc67294939ec2b1b95f86bac6701f634a0c74
1000miles <thuy.le@asquera.de>
2016-03-16 14:31:46 +0100

    Remove description text of nodes.feature from nodes_spec file

--- spec/features/nodes_spec.rb
+++ spec/features/nodes_spec.rb
@@ -3,43 +3,34 @@ require 'rails_helper'
 describe "Nodes Feature" do
   describe "Find, update and create nodes via API" do
     before do
-      # Given a provider "stiftung" exists with name: "Stiftung Gesundheit"
       provider = FactoryGirl.create(:provider, name: "Stiftung Gesundheit")
-      # And 1 valid pois exist
       poi = FactoryGirl.create(:poi)
-      # And the following provided_pois exist
       FactoryGirl.create(:provided_poi, wheelchair: "yes", url: "http://www.arzt-auskunft.de", poi: poi, provider: provider )
-      # When I am on the last poi's page
       visit(node_path(poi))
     end
 
     # Scenario: I want to see a decent title on the poi detail page
     describe "poi detail page" do
       it "has a decent title" do
-          # Then I should see "Dieser Ort ist auf Wheelmap.org: name, Bar, Essen & Trinken, Berlin" within "title"
           title_text = "Dieser Ort ist auf Wheelmap.org: name, Bar, Essen & Trinken, Berlin"
           expect(page.title).to eq(title_text)
       end
 
       # Scenario: I want to see a link to a landing page on the poi detail page
       it "has a link to a landing page" do
-        # Then I should see "Alle Orte vom Typ"
         expect(page).to have_link('Alle Orte vom Typ', href: "/map/berlin/bar/wheelchair/yes")
       end
 
       # Scenario: I want to see a back link to the root page
       it "has a back link to the root page" do
         save_page
-        # When I follow "zurück"
         click_link('zurück')
-        # Then I should be on the home page
         expect(current_path).to eq(root_path)
       end
     end
 
     # Scenario: I want to see a link to provider's detail page
     describe "provider detail page (e.g. Stiftung Gesundheit)" do
-      # Then I should see "Stiftung Gesundheit"
       it "has info box content: 'Stiftung Gesundheit'" do
         expect(page).to have_content("Stiftung Gesundheit")
       end
@@ -47,7 +38,6 @@ describe "Nodes Feature" do
       describe 'dropdown' do
         let(:selectbox){ all(:css, 'div.node-status-dropdown').first }
 
-        # And I should see "Voll Rollstuhlgerecht"
         it "has content 'voll rollstuhlgerecht'" do
           expect(selectbox).to have_content("Voll Rollstuhlgerecht")
         end
@@ -73,7 +63,6 @@ describe "Nodes Feature" do
 
       # Scenario: I want to see a wheelchair description
       it "has a wheelchair description" do
-        # Then I should see "Yes, we have a ramp."
         expect(page).to have_content("Yes, we have a ramp.")
       end
     end
