b84290519daaa5521902a5cf21ee0f258fe36f62
1000miles <thuy.le@asquera.de>
2016-04-11 16:13:36 +0000

    Refactor user_after_signup_spec.rb

--- spec/features/user_after_signup_spec.rb
+++ spec/features/user_after_signup_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 require 'shared_context/omniauth'
 require 'email_spec'
+require 'capybara/poltergeist'
+require 'pry'
 
 feature 'User after signup feature', :js => true do
   include_context "OmniAuth"
@@ -13,7 +15,7 @@ feature 'User after signup feature', :js => true do
       visit after_signup_edit_profile_path
     end
 
-    context "when user = horst, email = horst@wheelmap.org, password = password" do
+    context "when first_name is horst, email is horst@wheelmap.org & password is password" do
       given!(:user) { FactoryGirl.create(:user,
         first_name: 'horst',
         email: 'horst@wheelmap.org',
@@ -23,17 +25,18 @@ feature 'User after signup feature', :js => true do
         osm_id: 174
       )}
 
-      scenario "I set my email after signup without an email" do # added test
+      scenario "I set my email after signup without an email" do
         within("#user_email_input") do
           fill_in "user_email", with: ""
         end
-        # pending("JS alert: 'Du bekommst in wenigen Minuten eine E-Mail mit einem Link' instead of error")
         click_button 'Fertigstellen'
-        # expect(page).to raise_error("Bitte hinterlege eine E-Mail-Adresse vor dem Speichern.")
+        pending("Bug: 'Du bekommst in wenigen Minuten eine E-Mail mit einem Link' instead of error message")
+        # Test suggestion:
+        # expect { raise StandardError, 'Email muss ausgefüllt werden' }.to raise_error('Email muss ausgefüllt werden')
       end
     end
 
-    context "when user = horst2, email = horst@wheelmap.org, password = ''" do
+    context "when first_name is horst2, email is horst@wheelmap.org & password is empty" do
       given!(:user) { FactoryGirl.create(:user,
         first_name: 'horst2',
         email: 'horst@wheelmap.org',
@@ -45,30 +48,25 @@ feature 'User after signup feature', :js => true do
 
       # Scenario: I set my password after signup
       scenario "I set my password after signup" do
-        # binding.pry
         within("#user_email_input") do
          fill_in "user_email", with: ""
         end
-        # pending("Error: No password setting possible on after signup edit page. Needs correction!")
         click_button 'Fertigstellen'
-        expect(page).to have_content("Email muss ausgefüllt werden")
+        pending("Bug: No password setting possible on after signup edit page. Test implementation needs correction!")
       end
 
       # Scenario: I set my email after signup with existing password
-      scenario "I set my email after signup with another email address" do # text corrected
+      scenario "I set my email after signup with email address" do
         within("#user_email_input") do
-          fill_in "user_email", with: "peter@wheelmap.org" # peter@wheelmap.org
+          fill_in "user_email", with: "peter@wheelmap.org"
         end
-        pending
         click_button 'Fertigstellen'
         expect(page.current_path).to eq(root_path)
-        # Then 1 email should be delivered to "peter@wheelmap.org"
-        expect(last_email_sent).to deliver_to(user.email)
-        expect(last_email_sent).to have_subject("Noch einen Schritt zur Aktivierung deines Benutzerkontos")
+        expect(last_email_sent).to deliver_to('peter@wheelmap.org')
       end
     end
 
-    context "when user = horst, email = "", password = ''" do
+    context "when first_name is horst, email and password are left empty" do
       given!(:user) { FactoryGirl.create(:user,
         first_name: 'horst',
         email: "",
@@ -79,22 +77,21 @@ feature 'User after signup feature', :js => true do
       )}
 
       # Scenario: I set my email after signup without any prior email and get an welcome email
-      scenario "I set my email after signup without any prior email and get an welcome email" do
+      scenario "I set my email after signup without any prior email" do
         within("#user_email_input") do
          fill_in "user_email", with: "peter@wheelmap.org"
         end
-        pending
         click_button 'Fertigstellen'
         expect(page.current_path).to eq(root_path)
-        expect(last_email_sent).to deliver_to(user.email)
-        # Then 2 email should be delivered to "peter@wheelmap.org"
+        expect(last_email_sent).to deliver_to('peter@wheelmap.org')
+        expect(last_email_sent).to have_subject("Willkommen bei wheelmap.org")
       end
     end
   end
 
-  describe "User is not logged in" do
+  describe "is not logged in" do
   # "This test case is currently ignored, because registration/Login executes through OSM authentication and new user password page is not used anymore" :
-  # describe "User is logged out" do
+  # describe "User is not logged in" do
   #     scenario "I set my email after signup without any prior password" do
   #       visit destroy_user_session_path
   #       # When I am on the new user password page
