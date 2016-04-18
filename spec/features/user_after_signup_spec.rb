3413e80990381f3d025125d172a3d7b7342e1e79
1000miles <thuy.le@asquera.de>
2016-04-18 09:05:09 +0000

    Refactor User after signup feature & set to pending

--- spec/features/user_after_signup_spec.rb
+++ spec/features/user_after_signup_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 require 'shared_context/omniauth'
-require 'email_spec'
+require "email_spec"
 require 'capybara/poltergeist'
 require 'pry'
 
-feature 'User after signup feature', :js => true do
+feature 'User feature after signup', js: true do
   include_context "OmniAuth"
 
-  describe "User is logged in" do
+  describe "User is logged in & on after signup edit page" do
     background do
       visit new_user_session_path
       visit '/users/auth/osm'
@@ -15,7 +15,7 @@ feature 'User after signup feature', :js => true do
       visit after_signup_edit_profile_path
     end
 
-    context "when first_name is horst, email is horst@wheelmap.org & password is password" do
+    context "when first_name, email & password are given " do
       given!(:user) { FactoryGirl.create(:user,
         first_name: 'horst',
         email: 'horst@wheelmap.org',
@@ -25,18 +25,19 @@ feature 'User after signup feature', :js => true do
         osm_id: 174
       )}
 
-      scenario "I set my email after signup without an email" do
+      # additional test
+      scenario "I want to finalize my signup and click on button without changing the email" do
         within("#user_email_input") do
           fill_in "user_email", with: ""
         end
         click_button 'Fertigstellen'
-        pending("Bug: 'Du bekommst in wenigen Minuten eine E-Mail mit einem Link' instead of error message")
-        # Test suggestion:
-        # expect { raise StandardError, 'Email muss ausgefüllt werden' }.to raise_error('Email muss ausgefüllt werden')
+        pending("Bug: 'Du bekommst in wenigen Minuten eine E-Mail mit einem Link' instead of 'Email muss ausgefüllt werden'")
+        fail
+        expect { raise StandardError, 'Email muss ausgefüllt werden' }.to raise_error('Email muss ausgefüllt werden')
       end
     end
 
-    context "when first_name is horst2, email is horst@wheelmap.org & password is empty" do
+    context "when first_name & email are given" do
       given!(:user) { FactoryGirl.create(:user,
         first_name: 'horst2',
         email: 'horst@wheelmap.org',
@@ -47,26 +48,27 @@ feature 'User after signup feature', :js => true do
       )}
 
       # Scenario: I set my password after signup
-      scenario "I set my password after signup" do
+      scenario "I set my password" do
         within("#user_email_input") do
          fill_in "user_email", with: ""
         end
         click_button 'Fertigstellen'
-        pending("Bug: No password setting possible on after signup edit page. Test implementation needs correction!")
+        skip("Bug: No password setting possible on after signup edit page. Needs correction!")
+        fail
       end
 
       # Scenario: I set my email after signup with existing password
       scenario "I set my email after signup with email address" do
         within("#user_email_input") do
-          fill_in "user_email", with: "peter@wheelmap.org"
+          fill_in "user_email", with: 'peter@wheelmap.org'
         end
         click_button 'Fertigstellen'
-        expect(page.current_path).to eq(root_path)
         expect(last_email_sent).to deliver_to('peter@wheelmap.org')
       end
     end
 
-    context "when first_name is horst, email and password are left empty" do
+    # needs correction, because this case should not be possible
+    context "when only first_name is given" do
       given!(:user) { FactoryGirl.create(:user,
         first_name: 'horst',
         email: "",
@@ -90,16 +92,14 @@ feature 'User after signup feature', :js => true do
   end
 
   describe "is not logged in" do
-  # "This test case is currently ignored, because registration/Login executes through OSM authentication and new user password page is not used anymore" :
-  # describe "User is not logged in" do
-  #     scenario "I set my email after signup without any prior password" do
-  #       visit destroy_user_session_path
-  #       # When I am on the new user password page
-  #       # When I fill in "E-Mail" with "horst@wheelmap.org"
-  #       # And I press "Passwort neu setzen"
-  #       # And PENDING: make cached flash testable
-  #       # And I should see "Du bekommst in wenigen Minuten eine E-Mail mit Informationen wie du dein Passwort neu setzen kannst."
-  #     end
-  # end
+    scenario "I set my email after signup without any prior password" do
+      pending("BUG: This test case needs a revision since '/users/password/new' is not used anymore")
+      expect(current_path).to eq(new_user_password_path)
+      # When I am on the new user password page
+      # When I fill in "E-Mail" with "horst@wheelmap.org"
+      # And I press "Passwort neu setzen"
+      # And PENDING: make cached flash testable
+      # And I should see "Du bekommst in wenigen Minuten eine E-Mail mit Informationen wie du dein Passwort neu setzen kannst."
+    end
   end
 end
