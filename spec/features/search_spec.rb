7b3889c873cb8f36b571addc2a9328479c585ee9
1000miles <thuy.le@asquera.de>
2016-04-25 16:27:59 +0000

    Convert search-feature to spec

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -2,11 +2,18 @@ require 'rails_helper'
 require 'pry'
 
 # Feature: Search
-feature "Search Feature" do
+feature "Search Feature", type: :feature, js: true do
   background do
     # Given I am on the home page
     visit root_path
-    find('button.go').click
+    using_wait_time 10 do
+      expect(page).to have_css('div.overlay-wrapper div.overlay')
+      div = find('div.overlay-wrapper div.overlay')
+      expect(div).to have_xpath('//button')
+      div.find('button').click
+      # And search field is rendered with ember.js
+      expect(page).to have_css('div#wheelmap div.ember-view')
+    end
   end
 
   describe "Simple search" do
@@ -29,15 +36,16 @@ feature "Search Feature" do
 
     let(:search_input) { FactoryGirl.create(:css, 'div.input-append') }
 
-    it "is possible to search for 'Herborn in search input field'" do
-      pending
-      within ('#search') do
-        # And I search for "Herborn"
-        fill_in 'value', with: 'herborn'
-      end
+    # And I search for "Herborn"
+    it "is possible to search for 'Herborn'" do
+      expect(page).to have_css('input#search')
+      search = find(:css, 'input#search')
+      search.set('herborn')
+      expect(search.value).to eq('herborn')
       click_button('Finden')
+
       # Then I should be on the search result page
-      expect(page).to redirect_to('/search?q=herborn')
+      expect(page.current_url).to end_with('/search?q=herborn')
     end
   end
 end
