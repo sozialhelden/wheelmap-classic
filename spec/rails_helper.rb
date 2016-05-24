1601d89dbc89f0ad034fd7b5c9b54df46585085e
1000miles <thuy.le@asquera.de>
2016-05-24 18:17:45 +0000

    Pin Shoulda-Matchers & add gem to rails_helper.rb.
    Fix for #241: belong_to, have_many, validate_presebce_of, validate_uniqueness_of

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@
   require 'factory_girl'
   require 'webmock/rspec'
   require 'carrierwave/test/matchers'
+  require 'shoulda-matchers'
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
@@ -56,4 +57,21 @@
 
     # replace deprecated ExampleGroup#example
     config.expose_current_running_example_as :example
+  end
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      # Choose a test framework:
+      with.test_framework :rspec
+      # with.test_framework :minitest
+      # with.test_framework :minitest_4
+      # with.test_framework :test_unit
+
+      # Choose one or more libraries:
+      # with.library :active_record
+      # with.library :active_model
+      # with.library :action_controller
+      # Or, choose the following (which implies all of the above):
+      with.library :rails
+    end
   end
