9912fae02a79463ad90e200d444b04d53b521298
Thuy Le <thuy.le@asquera.de>
2016-05-31 11:36:37 +0000

    Disable the compressors :yui and :uglifier
    We disable the 2 gems for now but will take a look into compressor alternatives later as :yui seems to cause trouble in the precompiling process.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,15 +17,16 @@ Wheelmap::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  # Compress JS and CSS faster in production
-  config.assets.css_compressor = :yui
-  config.assets.js_compressor = :uglifier
+  # @TODO Enable compressors and find alternative for :yui
+  # Compress CSS and JS faster in production
+  # config.assets.css_compressor = :yui
+  # config.assets.js_compressor = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
-  config.assets.digest = true
+  config.assets.digest = false
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
