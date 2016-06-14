6bd0e8a370d7fc6e2725ef0922d2235c612a921f
Jan Schulte <jan.schulte@asquera.de>
2016-06-14 14:37:12 +0000

    Enable asset compression via uglifier

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -16,6 +16,7 @@ Wheelmap::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
+  config.assets.js_compressor = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
