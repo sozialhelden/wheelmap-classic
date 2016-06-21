3b7d70d421123d7f03dc4da6add017b5b6042450
Lennart Hildebrandt <hello@lennerd.com>
2016-06-21 12:41:45 +0200

    Fix I18n test.

--- app/assets/javascripts/common/I18n.js
+++ app/assets/javascripts/common/I18n.js
@@ -1,47 +1,54 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import classNames from 'classnames';
 
 const { string } = PropTypes;
 
-function I18n({ scope, className, ...props }) {
-  let translation = I18n.t(scope);
-  let missingTranslation = false;
-  let title;
+class I18n extends Component {
+  static propTypes = {
+    scope: string.isRequired,
+    className: string
+  };
 
-  // Translation was not found
-  if (translation == null) {
-    translation = scope.replace(/\./g, ' ');
-    missingTranslation = true;
-    title = 'translation missing';
+  static get locale() {
+    return global.I18n.locale;
   }
 
-  // Use dangerouslySetInnerHTML as the translation string as translations can contain html elements
-  function createMarkup() {
-    return { __html: translation };
+  static t(scope) {
+    const translation = global.I18n.t(scope);
+
+    if (translation.indexOf(scope) > -1) {
+      return null;
+    }
+
+    return translation;
   }
 
-  className = classNames(className, { 'translation-missing': missingTranslation });
+  render() {
+    const { scope, ...props } = this.props;
+    let { className } = this.props;
 
-  return (
-    <span className={className} title={title} dangerouslySetInnerHTML={createMarkup()} {...props} />
-  );
+    let translation = I18n.t(scope);
+    let missingTranslation = false;
+    let title;
+
+    // Translation was not found
+    if (translation == null) {
+      translation = scope.replace(/\./g, ' ');
+      missingTranslation = true;
+      title = 'translation missing';
+    }
+
+    // Use dangerouslySetInnerHTML as the translation string as translations can contain html elements
+    function createMarkup() {
+      return { __html: translation };
+    }
+
+    className = classNames(className, { 'translation-missing': missingTranslation });
+
+    return (
+      <span {...props} className={className} title={title} dangerouslySetInnerHTML={createMarkup()} />
+    );
+  }
 }
-
-I18n.t = (scope) => {
-  const translation = global.I18n.t(scope);
-
-  if (translation.indexOf(scope) > -1) {
-    return null;
-  }
-
-  return translation;
-};
-
-I18n.locale = global.I18n.locale;
-
-I18n.propTypes = {
-  scope: string.isRequired,
-  className: string
-};
 
 export default I18n;
