56636d8d6d4c71f90ecf07f9c2a04b2147d89aff
Lennart Hildebrandt <hello@lennerd.com>
2016-06-28 13:35:58 +0200

    Fix setParams error and use built in query string dependency.

--- app/assets/javascripts/common/helpers/query.js
+++ app/assets/javascripts/common/helpers/query.js
@@ -1,1 +1,16 @@
-export { stringify, parse as parseString } from 'query-string';
+import { stringify, parse } from 'query-string';
+
+export {
+  stringify,
+  parse as parseString
+};
+
+export function insert(url, query) {
+  const queryString = stringify(query);
+
+  if (queryString === '') {
+    return url;
+  }
+
+  return `${url}?${queryString}`;
+}
