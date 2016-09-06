23a0ee765a8d9341f5b02638b1ab845193870137
Andrew Hobden <andrew@hoverbear.org>
2016-09-06 16:05:58 +0200

    Point cap at the right branch

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,7 +6,7 @@ set :rails_env, "staging" #added for delayed job
 set :stage, :staging
 set :deploy_to, "/var/apps/#{fetch(:application)}/"
 
-set :branch, :"feature/infra-rebuild"
+set :branch, :"feature/infrastructure-rebuild/manifests-to-apps"
 set :rev, proc { `git rev-parse --short #{fetch(:branch)}`.chomp }
 
 # Simple Role Syntax
