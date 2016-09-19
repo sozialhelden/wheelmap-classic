233159e8abbb874bb2c659a6f2d592504f574a67
Thuy Le <thuy.le@asquera.de>
2016-09-19 14:42:44 +0000

    Ensure that `name` is present

--- app/models/community_support_request.rb
+++ app/models/community_support_request.rb
@@ -1,5 +1,8 @@
 class CommunitySupportRequest
   include ActiveModel::Validations
+  attr_accessor :name
+  
+  validates_presence_of :name
 
   def initialize(name, email, message)
     @name = name
--- spec/models/community_support_request_spec.rb
+++ spec/models/community_support_request_spec.rb
@@ -5,4 +5,11 @@ describe CommunitySupportRequest do
     community_support_request = CommunitySupportRequest.new("Holger", "holger@example.com", "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.")
     expect(community_support_request.valid?).to be true
   end
+
+  context "with invalid attributes" do
+    it "is not valid when name is missing" do
+      community_support_request = CommunitySupportRequest.new(nil, "holger@example.com", "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.")
+      expect(community_support_request.valid?).to be false
+    end
+  end
 end
