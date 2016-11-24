523d442d8fe31e966e07c6b9322d846296ce4bcd
Thuy Le <thuy.le@asquera.de>
2016-11-24 09:45:41 +0000

    Set assets compiling to true for staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -19,7 +19,7 @@ Wheelmap::Application.configure do
   config.assets.js_compressor = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
