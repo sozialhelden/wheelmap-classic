eeb06037adb540d6fc472a34369edcf87b324961
Thuy Le <thuy.le@asquera.de>
2016-11-28 14:47:37 +0000

    Rename NodeType class back to LocalNodeType to use own model for migration

--- db/migrate/20161128102235_change_art_museum_to_museum_art.rb
+++ db/migrate/20161128102235_change_art_museum_to_museum_art.rb
@@ -6,7 +6,7 @@ class ChangeArtMuseumToMuseumArt < ActiveRecord::Migration
 
   # Rename art_museum.png to museum_art.png but keep same identifier & osm_value
   def up
-    node_type = NodeType.find_by(identifier: 'art', osm_value: 'art', icon: 'art_museum.png')
+    node_type = LocalNodeType.find_by(identifier: 'art', osm_value: 'art', icon: 'art_museum.png')
     unless node_type.nil? && node_type.icon == 'art_museum.png'
       node_type.icon = 'museum_art.png'
       node_type.save
@@ -16,7 +16,7 @@ class ChangeArtMuseumToMuseumArt < ActiveRecord::Migration
   end
 
   def down
-    node_type = NodeType.find_by(identifier: 'art', osm_value: 'art', icon: 'museum_art.png')
+    node_type = LocalNodeType.find_by(identifier: 'art', osm_value: 'art', icon: 'museum_art.png')
     unless node_type.nil? && node_type.icon == 'museum_art.png'
       node_type.icon = 'art_museum.png'
       node_type.save
