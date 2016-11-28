75a88022334efdef58797b56dbe40110c4d82385
Thuy Le <thuy.le@asquera.de>
2016-11-28 14:14:13 +0000

    Specify query by chaining more filters and refactor overwriting condition
    Related Github feedback:
    https://github.com/sozialhelden/wheelmap/pull/486#discussion_r89775493
    https://github.com/sozialhelden/wheelmap/pull/486#discussion_r89776041

--- db/migrate/20161128102235_change_art_museum_to_museum_art.rb
+++ db/migrate/20161128102235_change_art_museum_to_museum_art.rb
@@ -1,27 +1,27 @@
 class ChangeArtMuseumToMuseumArt < ActiveRecord::Migration
   class LocalNodeType < ActiveRecord::Base
     self.table_name = 'node_types'
-    validates :icon, presence: true
+    validates :identifier, :osm_value, :icon, presence: true
   end
 
   # Rename art_museum.png to museum_art.png but keep same identifier & osm_value
   def up
-    node_type = LocalNodeType.find_by(id: 153)
-    if node_type.nil?
-      return
-    else
+    node_type = NodeType.find_by(identifier: 'art', osm_value: 'art', icon: 'art_museum.png')
+    unless node_type.nil? && node_type.icon == 'art_museum.png'
       node_type.icon = 'museum_art.png'
       node_type.save
+    else
+      return
     end
   end
 
   def down
-    node_type = LocalNodeType.find_by(id: 153)
-    if node_type.nil?
-      return
-    else
+    node_type = NodeType.find_by(identifier: 'art', osm_value: 'art', icon: 'museum_art.png')
+    unless node_type.nil? && node_type.icon == 'museum_art.png'
       node_type.icon = 'art_museum.png'
       node_type.save
+    else
+      return
     end
   end
 end
