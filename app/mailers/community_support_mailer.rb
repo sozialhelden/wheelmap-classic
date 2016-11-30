55c9c4b0240bee34d94667cbe551b8721df94689
Jan Schulte <jan.schulte@asquera.de>
2016-11-30 16:04:12 +0000

    Change email subject

--- app/mailers/community_support_mailer.rb
+++ app/mailers/community_support_mailer.rb
@@ -4,6 +4,6 @@ class CommunitySupportMailer < ActionMailer::Base
   def send_to_support_team(community_support_request)
     @community_support_request = community_support_request
 
-    mail(:to => "bugs@wheelmap.org", :subject => "Bugreport von wheelmap.org")
+    mail(:to => "bugs@wheelmap.org", :subject => "Help request from wheelmap.org")
   end
 end
