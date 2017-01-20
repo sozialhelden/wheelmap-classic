bfd3f0e9ef08f629339ea3a94ae9c1d0911e7130
Jan Schulte <jan.schulte@asquera.de>
2017-01-20 15:44:16 +0000

    Change `consider_all_requests_local` back to false
    This was set to true for debugging purposes and was only accidentally
    committed.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -6,7 +6,7 @@ Wheelmap::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Specifies the header that your server uses for sending files
