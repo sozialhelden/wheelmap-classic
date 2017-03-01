a1f4affd96ae7c680ba33d29496f2e6b98d2fd7f
Jan Schulte <jan.schulte@asquera.de>
2017-03-01 13:01:56 +0000

    Include missing i18n depedencies

--- app/assets/javascripts/language_picker_styling.js
+++ app/assets/javascripts/language_picker_styling.js
@@ -1,5 +1,7 @@
 //= require jquery
 //= require bootstrap
 //= require bootstrap-select
+//= require i18n
+//= require i18n-config
 
 $(document).ready(function () { $('.selectpicker').selectpicker(); });
