fcee8ca3f28d9e0d9f6ccd1f0248bf806e681989
Thuy Le <thuy.le@asquera.de>
2017-09-26 16:21:43 +0000

    Refactoring: Use filter instead of ignore (new syntax)

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -4,9 +4,14 @@ Airbrake.configure do |config|
   config.project_id = ENV['AIRBRAKE_PROJECT_ID']
   config.project_key = ENV['AIRBRAKE_API_KEY']
   config.environment = Rails.env
-  config.ignore << 'Rosemary::Conflict'
-  config.ignore << 'Rosemary::ServerError'
-  config.ignore << 'Rosemary::Gone'
-  config.ignore << 'Rosemary::Unavailable'
-  config.ignore << 'Net::ReadTimeout'
+
+  Airbrake.add_filter do |notice|
+    notice.ignore! if notice.stash[:exception].is_a?([
+      'Rosemary::Conflict',
+      'Rosemary::ServerError',
+      'Rosemary::Gone',
+      'Rosemary::Unavailable',
+      'Net::ReadTimeout'
+    ])
+  end
 end
