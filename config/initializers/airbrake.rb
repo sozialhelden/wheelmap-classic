8000e7a921c7f1a7cbe5f6d16341c82ea3ac8496
Thuy Le <thuy.le@asquera.de>
2017-09-27 15:11:20 +0000

    Update airbrake config
    - ignore test environment
    - specify root directory
    - do not show password and authorization keys
    - integrate airbrake errors to Rails logger

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,9 +1,14 @@
 require 'airbrake-ruby'
 
+# https://github.com/airbrake/airbrake-ruby#configuration
 Airbrake.configure do |config|
   config.project_id = ENV['AIRBRAKE_PROJECT_ID']
   config.project_key = ENV['AIRBRAKE_API_KEY']
   config.environment = Rails.env
+  config.ignore_environments = %w(test)
+  config.root_directory = Rails.root
+  config.blacklist_keys = [/password/i, /authorization/i]
+  config.logger = Rails.logger
 
   Airbrake.add_filter do |notice|
     notice.ignore! if notice.stash[:exception].is_a?([
